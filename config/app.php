b9d57724ab4ebe2bf89b90781d633ee0b2c4479d
Thijs Dortmann <thijs@thzd.nl>
2016-01-13 21:54:13 +0100

    Install laravelcollective HTML forms

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
          * External Service Providers
          */
         Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -198,6 +199,9 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
 
     ],
 
