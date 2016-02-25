29f632a99bee89c471205a9103e1c3b409a1562b
Jonathan Juursema <github@jonathanj.nl>
2016-02-25 17:32:15 +0100

    Added update / install script.

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+echo "Starting update...";
+
+# Enable maintenance mode.
+php artisan down
+
+# Pull the new data.
+git pull
+
+# By default we update dependencies, but we can disable them.
+if [ "$1" == --no-deps ] || [ "$2" == --no-deps ]; then
+else
+  composer self-update
+  composer install
+fi
+
+# Always clear the cache.
+php artisan cache:clear
+
+# Should we update permissions?
+if [ "$1" == --no-permissions ] || [ "$2" == --no-permissions ]; then
+else
+  chmod -R ug+rwx storage bootstrap/cache
+fi
+
+# Do migrations.
+php artisan migrate
+
+# Disable maintenance mode.
+php artisan up
+
+# Notify end
+echo "Done!";
