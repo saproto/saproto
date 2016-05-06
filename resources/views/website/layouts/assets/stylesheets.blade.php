18fc069692e8c7c072c842b5f87851381c060e8f
Jonathan Juursema <github@jonathanj.nl>
2016-05-06 22:49:55 +0200

    Rewrote js/css dependencies. Now using bower + elixr.

--- resources/views/website/layouts/assets/stylesheets.blade.php
+++ resources/views/website/layouts/assets/stylesheets.blade.php
@@ -1,3 +1,2 @@
-<link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
-
-<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="{{ asset('assets/bootstrap.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/vendor.css') }}">
