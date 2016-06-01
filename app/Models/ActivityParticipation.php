d22d2d85b10a56d42e096218cfd70a24ea765d50
Jonathan Juursema <github@jonathanj.nl>
2016-06-01 22:29:18 +0200

    Added models for activity participation.

---
+++ app/Models/ActivityParticipation.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Proto\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CommitteeMembership extends Validatable
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'activities_users';
+
+    /**
+     * @return mixed The user this association is for.
+     */
+    public function user() {
+        return $this->belongsTo('Proto\Models\User');
+    }
+
+    /**
+     * @return mixed The activity this association is for.
+     */
+    public function activity() {
+        return $this->belongsTo('Proto\Models\Activity');
+    }
+
+    protected $fillable = ['user_id', 'committee_id', 'role', 'edition', 'start', 'end'];
+
+    protected $rules = array(
+        'user_id' => 'required|integer',
+        'activity_id' => 'required|integer',
+        'committees_activities_id' => 'required|integer'
+    );
+}
