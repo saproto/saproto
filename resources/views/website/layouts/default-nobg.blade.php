af19f5389bf8d4ee2aaa08c4ad4ad0e8e5238b37
Jonathan Juursema <github@jonathanj.nl>
2016-06-21 17:43:52 +0200

    Moved flash messsages to modal.

--- resources/views/website/layouts/default-nobg.blade.php
+++ resources/views/website/layouts/default-nobg.blade.php
@@ -4,20 +4,6 @@
 
     <div id="container" class="container container-nobg">
 
-        @if (Session::has('flash_message'))
-            <div class="alert alert-info alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                {{ Session::get('flash_message') }}
-            </div>
-        @endif
-
-        @foreach($errors->all() as $e)
-            <div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                {{ $e }}
-            </div>
-        @endforeach
-
         @yield('content')
 
     </div>
--- resources/views/website/layouts/panel.blade.php
+++ resources/views/website/layouts/panel.blade.php
@@ -4,20 +4,6 @@
 
     <div id="container" class="container container-nobg">
 
-        @if (Session::has('flash_message'))
-            <div class="alert alert-info alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                {{ Session::get('flash_message') }}
-            </div>
-        @endif
-
-        @foreach($errors->all() as $e)
-            <div class="alert alert-danger alert-dismissable">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                {{ $e }}
-            </div>
-        @endforeach
-
         <div class="col-md-6 col-md-offset-3 col-xs-12 col-xs-offset-0">
 
             <div class="panel panel-default">
