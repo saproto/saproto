fecf4335ec2231182bb882e680e2db0127afc937
Thijs Dortmann <thijs@thijsdortmann.nl>
2016-07-04 22:01:11 +0200

    Add softdelete to page model dependencies.

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -3,8 +3,11 @@
 namespace Proto\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Page extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'pages';
 }
