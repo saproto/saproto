0bd1c25182ea8ebc8d72b3ea525862d806c2eab8
Jonathan Juursema <github@jonathanj.nl>
2016-09-09 16:48:42 +0200

    Better error reporting for users and dev (via Sentry).

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,10 +3,9 @@
 namespace Proto\Exceptions;
 
 use Exception;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
+use App;
 
 class Handler extends ExceptionHandler
 {
@@ -25,27 +24,33 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Exception  $e
+     * @param  \Exception $e
      * @return void
      */
     public function report(Exception $e)
     {
-        return parent::report($e);
+
+        if (App::environment('production')) {
+            app('sentry')->captureException($e);
+        } else {
+            return parent::report($e);
+        }
     }
 
     /**
      * Render an exception into an HTTP response.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $e
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Exception $e
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $e)
     {
-        if ($e instanceof ModelNotFoundException) {
-            $e = new NotFoundHttpException($e->getMessage(), $e);
-        }
 
-        return parent::render($request, $e);
+        if (App::environment('production')) {
+            return response()->view('errors.generic', [], 500);
+        } else {
+            return parent::render($request, $e);
+        }
     }
 }
--- resources/views/errors/generic.blade.php
+++ resources/views/errors/generic.blade.php
@@ -1,9 +1,41 @@
-@extends('website.layouts.panel')
+@extends('website.layouts.default')
 
-@section('panel-title')
-    @yield('errornumber'): @yield('errorshort')
+@section('page-title')
+    Something went horribly wrong. :(
 @endsection
 
-@section('panel-body')
-    @yield('errormessage')
+@section('content')
+
+    <div class="row">
+
+        <div class="col-md-8">
+
+            <h3>
+                Hey {{ (Auth::check() ? Auth::user()->name_first : 'there') }}!
+            </h3>
+
+            <p>
+                <strong>
+                    We encountered an error while trying to process your request.
+                </strong>
+            </p>
+
+            <p>
+                We're very sorry, but something went horribly wrong while we tried to do what you requested. Don't
+                worry, this sometimes happens. We make use of an automatic error-logging system, so your issue has been
+                recorded, will be investigated and usually solved. If, despite all this, you're still looking to ask
+                somebody what the heck just went wrong, you can always contact the <a href="{{ route('developers') }}">webmasters</a>
+                of this website.
+            </p>
+
+        </div>
+
+        <div class="col-md-4" style="text-align: center;">
+
+            <img src="{{ asset('images/application/exception.png') }}">
+
+        </div>
+
+    </div>
+
 @endsection
