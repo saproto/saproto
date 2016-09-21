dfcc7b63a8c211083708d13b608a0aefe28dc1a9
Thijs Dortmann <thijs@thijsdortmann.nl>
2016-09-21 19:27:29 +0200

    Fix error in migration.

--- database/migrations/2016_06_29_211636_update_achievement_img_id_nullable.php
+++ database/migrations/2016_06_29_211636_update_achievement_img_id_nullable.php
@@ -12,7 +12,7 @@ class UpdateAchievementImgIdNullable extends Migration
      */
     public function up()
     {
-        Schema::table('achievements', function ($table) {
+        Schema::table('achievement', function ($table) {
             $table->integer('img_file_id')->nullable()->change();
         });
     }
@@ -24,7 +24,7 @@ class UpdateAchievementImgIdNullable extends Migration
      */
     public function down()
     {
-        Schema::table('achievements', function ($table) {
+        Schema::table('achievement', function ($table) {
             $table->integer('img_file_id')->nullable(false)->change();
         });
     }
