7fc811a43c6f7d2a258eacc6a6386278211acac9
Thijs Dortmann <thijs@thijsdortmann.nl>
2016-09-21 19:31:00 +0200

    Fix table name achievements.

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Achievement extends Model
 {
-    protected $table = 'achievements';
+    protected $table = 'achievement';
 
     protected $fillable = ['name', 'desc', 'img_file_id', 'tier'];
 
