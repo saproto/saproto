c93145aafd421b3c46bbc1e0186e25b00282152d
Pepijn Peeters <p.t.m.peeters@student.utwente.nl>
2016-11-14 23:42:19 +0100

    overhaul for how users see achievements on profiles

--- resources/views/users/profile/achieved.blade.php
+++ resources/views/users/profile/achieved.blade.php
@@ -2,15 +2,15 @@
 
 @if(count($user->achieved()) > 0)
 
-    <ul class="list-group">
+    <ul class="achievement-list">
 
         @foreach($user->achieved() as $key => $achievement)
 
-            <li class="list-group-item achievement {{ $achievement->tier }}">
+            <li class="achievement {{ $achievement->tier }}">
 
-                @if(Auth::check() && Auth::user()->can("board"))
-                    <a class="del" href="{{ route('achievement::take', ['id' => $achievement->id, 'user' => $user->id]) }}">Remove</a>
-                @endif
+                <div class="achievement-label">
+                    <img src="{{ asset('images/achievements/' . strtolower($achievement->tier) . '.svg') }}" alt="">
+                </div>
 
                 <div class="achievement-icon">
                     @if($achievement->image)
@@ -19,10 +19,40 @@
                         No icon available
                     @endif
                 </div>
-                <div>
-                    <strong>{{ $achievement->name }}</strong>
-                    <p>{{ $achievement->desc }}</p>
-                    <sub>Acquired on {{ $achievement->pivot->created_at->format('d/m/Y') }}.</sub>
+
+                <div class="achievement-tooltip">
+
+                    <div class="achievement-button">
+                        <img src="{{ asset('images/achievements/' . strtolower($achievement->tier) . '_tooltip.svg') }}" alt="">
+                        <div class="achievement-button-icon">
+                            @if($achievement->image)
+                                <img src="{!! $achievement->image->generateImagePath(700,null) !!}" alt="">
+                            @else
+                                No icon available
+                            @endif
+                        </div>
+                    </div>
+
+                    <div class="achievement-text">
+
+                        <div class="achievement-title">
+                            <strong>{{ $achievement->name }}</strong>
+                        </div>
+
+                        <div class="achievement-desc">
+                            <p>{{ $achievement->desc }}</p>
+                        </div>
+
+                        <div class="achievement-data">
+                            @if(Auth::check() && Auth::user()->can("board"))
+                                <sub>Acquired on {{ $achievement->pivot->created_at->format('d/m/Y') }}.</sub>
+                                <a class="del"
+                                   href="{{ route('achievement::take', ['id' => $achievement->id, 'user' => $user->id]) }}">Remove</a>
+                            @endif
+                        </div>
+
+                    </div>
+
                 </div>
 
             </li>
