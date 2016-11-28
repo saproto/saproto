e490a6218ac72d34f52656e6cb94c08577a02672
Bouke Regnerus <bouke@regner.us>
2016-11-28 23:07:36 +0100

    Fixes

--- resources/views/website/home/shared.blade.php
+++ resources/views/website/home/shared.blade.php
@@ -105,6 +105,74 @@
 
         </div>
 
+        <div class="container">
+            <div id="myCarousel" class="carousel slide" data-ride="carousel">
+
+              <!-- Wrapper for slides -->
+              <div class="carousel-inner">
+
+                <div class="item active">
+                  <img src="http://placehold.it/760x400/cccccc/ffffff">
+                   <div class="carousel-caption">
+                    <h4><a href="#">Lorem ipsum dolor sit amet consetetur sadipscing</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                  </div>
+                </div><!-- End Item -->
+
+                 <div class="item">
+                  <img src="http://placehold.it/760x400/999999/cccccc">
+                   <div class="carousel-caption">
+                    <h4><a href="#">consetetur sadipscing elitr, sed diam nonumy eirmod</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                  </div>
+                </div><!-- End Item -->
+
+                <div class="item">
+                  <img src="http://placehold.it/760x400/dddddd/333333">
+                   <div class="carousel-caption">
+                    <h4><a href="#">tempor invidunt ut labore et dolore</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                  </div>
+                </div><!-- End Item -->
+
+                <div class="item">
+                  <img src="http://placehold.it/760x400/999999/cccccc">
+                   <div class="carousel-caption">
+                    <h4><a href="#">magna aliquyam erat, sed diam voluptua</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                  </div>
+                </div><!-- End Item -->
+
+                <div class="item">
+                  <img src="http://placehold.it/760x400/dddddd/333333">
+                   <div class="carousel-caption">
+                    <h4><a href="#">tempor invidunt ut labore et dolore magna aliquyam erat</a></h4>
+                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat. <a class="label label-primary" href="http://sevenx.de/demo/bootstrap-carousel/" target="_blank">Free Bootstrap Carousel Collection</a></p>
+                  </div>
+                </div><!-- End Item -->
+              </div><!-- End Carousel Inner -->
+
+            <ul class="list-group col-sm-6">
+              <li data-target="#myCarousel" data-slide-to="0" class="list-group-item active"><h4>Lorem ipsum dolor sit amet consetetur sadipscing</h4></li>
+              <li data-target="#myCarousel" data-slide-to="1" class="list-group-item"><h4>consetetur sadipscing elitr, sed diam nonumy eirmod</h4></li>
+              <li data-target="#myCarousel" data-slide-to="2" class="list-group-item"><h4>tempor invidunt ut labore et dolore</h4></li>
+              <!-- <li data-target="#myCarousel" data-slide-to="3" class="list-group-item"><h4>magna aliquyam erat, sed diam voluptua</h4></li>
+              <li data-target="#myCarousel" data-slide-to="4" class="list-group-item"><h4>tempor invidunt ut labore et dolore magna aliquyam erat</h4></li> -->
+            </ul>
+
+              <!-- Controls -->
+              <div class="carousel-controls">
+                  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                  </a>
+                  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                  </a>
+              </div>
+
+            </div><!-- End Carousel -->
+        </div>
+
         <hr>
 
         <h1 style="text-align: center; color: #fff; margin: 30px;">
@@ -145,7 +213,33 @@
         var sliderDelta = 0;
         var oneWayOrTheOther = true;
 
+        $(window).load(function() {
+            var boxheight = $('#myCarousel .carousel-inner').innerHeight();
+            var itemlength = $('#myCarousel .item').length;
+            var triggerheight = Math.round(boxheight/itemlength+1);
+        	$('#myCarousel .list-group-item').outerHeight(triggerheight);
+        });
+
         $(document).ready(function () {
+            var clickEvent = false;
+            $('#myCarousel').carousel({
+                interval:   4000
+            }).on('click', '.list-group li', function() {
+                    clickEvent = true;
+                    $('.list-group li').removeClass('active');
+                    $(this).addClass('active');
+            }).on('slid.bs.carousel', function(e) {
+                if(!clickEvent) {
+                    var count = $('.list-group').children().length -1;
+                    var current = $('.list-group li.active');
+                    current.removeClass('active').next().addClass('active');
+                    var id = parseInt(current.data('slide-to'));
+                    if(count == id) {
+                        $('.list-group li').first().addClass('active');
+                    }
+                }
+                clickEvent = false;
+            });
 
             setTimeout(function () {
 
