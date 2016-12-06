c07fd8215f811d0d969c0d346f91c55d22118b7e
thijsdortmann <thijs@thijsdortmann.nl>
2016-12-06 11:25:09 +0100

    Added cronjob for flickrsync.

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,5 +37,6 @@ class Kernel extends ConsoleKernel
         $schedule->command('proto:emailcron')->everyMinute();
         $schedule->command('proto:newslettercron')->weekly()->mondays()->at('11:00');
         $schedule->command('proto:birthdaycron')->daily()->at('00:01');
+        $schedule->command('proto:flickrsync')->everyThirtyMinutes();
     }
 }
