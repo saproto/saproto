773a0ec2e31f594b1fd464e8be4542423fa3f22e
Jonathan Juursema <github@jonathanj.nl>
2017-03-06 13:33:06 +0100

    Fixes bug where member could not be added due to CSRF protection.

--- resources/views/users/admin/add.blade.php
+++ resources/views/users/admin/add.blade.php
@@ -1,6 +1,7 @@
 <div class="modal fade" id="addMembership" tabindex="-1" role="dialog" aria-labelledby="addMembershipLabel">
     <div class="modal-dialog" role="document">
         <form action="{{ route("user::member::add", ['id'=>$user->id]) }}" method="post">
+            {!! csrf_field() !!}
             <div class="modal-content">
                 <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
