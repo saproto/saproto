0742276aeeac8da00b2ba1cf08de9efb96d815fe
Jonathan Juursema <github@jonathanj.nl>
2017-03-22 22:08:14 +0100

    Update .gitignore to allow developers to have some local CSS.

--- resources/views/website/layouts/assets/stylesheets.blade.php
+++ resources/views/website/layouts/assets/stylesheets.blade.php
@@ -1,9 +1,1 @@
 <link rel="stylesheet" href="{{ asset('assets/application.min.css') }}">
-
-@if(!$app->environment('production'))
-    <style type="text/css">
-        /*#wrap {
-            background-image: repeating-linear-gradient(-45deg, transparent, transparent 25px, rgba(0,0,0,.1) 25px, rgba(0,0,0,.1) 50px);
-        }*/
-    </style>
-@endif
