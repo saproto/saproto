c93b0db0ca6fabbc6a153590543554369fe00863
Jonathan Juursema <github@jonathanj.nl>
2017-09-18 21:00:32 +0200

    Removed attachments box from page view. (Fixes #521)

--- resources/views/pages/show.blade.php
+++ resources/views/pages/show.blade.php
@@ -8,49 +8,25 @@
 
     <div class="row">
 
-        <div class="@if($page->featuredImage || $page->files->count() > 0) col-md-8 @else col-md-12 @endif">
+        <div class="@if($page->featuredImage) col-md-8 @else col-md-12 @endif">
 
-                <div class="page-show__content">
-                    {!! $parsedContent !!}
-                </div>
-
+            <div class="page-show__content">
+                {!! $parsedContent !!}
             </div>
 
-        @if($page->featuredImage || $page->files->count() > 0)
+        </div>
+
+        @if($page->featuredImage)
 
             <div class="col-md-4">
 
                 @if($page->featuredImage)
                     <div class="panel panel-default">
-                        <img src="{{ $page->featuredImage->generateImagePath('600', null) }}" class="img-responsive" />
+                        <img src="{{ $page->featuredImage->generateImagePath('600', null) }}" class="img-responsive"/>
                     </div>
                 @endif
 
-                    @if($page->files->count() > 0)
-
-                    <div class="panel panel-default">
-
-                        <div class="panel-heading">
-
-                            Attachments
-
-                        </div>
-
-                        <div class="panel-body">
-
-                            @foreach($page->files as $file)
-
-                                <p><i class="fa fa-paperclip" aria-hidden="true"></i> <a href="{{ $file->generatePath() }}" target="_blank">{{ $file->original_filename }}</a></p>
-
-                            @endforeach
-
-                        </div>
-
-                    </div>
-
-                    @endif
-
-                </div>
+            </div>
 
         @endif
 
