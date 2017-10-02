e0049526f9a6ceec15c3c4f4b2bea62e67b7e579
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2017-10-02 23:22:52 +0200

    Temporary auto reload because the screen is hosted on the Pi.

--- resources/views/smartxp/boardroom.blade.php
+++ resources/views/smartxp/boardroom.blade.php
@@ -6,6 +6,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no"/>
+    
+    <meta http-equiv="refresh" content="3600">
 
     <link rel="shortcut icon" href="{{ asset('images/favicons/favicon'.mt_rand(1, 4).'.png') }}"/>
 
