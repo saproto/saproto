280d291ee7b6963f925b342dad908676f7b63594
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2017-11-06 22:43:26 +0100

    Updated manual e-mail template.

--- resources/views/emails/manualemail.blade.php
+++ resources/views/emails/manualemail.blade.php
@@ -23,11 +23,11 @@
                     ){{ ($i + 1 == count($destination) ? '' : ', ') }}
                 @endforeach
             @elseif($destination == 'event')
-                This is an e-mail directed to all participants of the activity {{ $event_name }}.
+                This is an e-mail directed to all participants of the {{ $event_name }} activity. You receive this e-mail because you signed up as a participant.
             @elseif($destination == 'users')
-                This is an e-mail directed to all user accounts on the S.A. Proto website.
+                This is an e-mail directed to all user accounts on the website of S.A. Proto. You receive this e-mail because you have an active user account.
             @elseif($destination == 'members')
-                This is an e-mail directed to all members off the S.A. Proto website.
+                This is an e-mail directed to all members of S.A. Proto. You receive this e-mail because you have an active membership.
             @endif
         </sub>
     </p>
