c3b3ca043aed06d0e05575c3f5eda6590f7ef244
Jonathan Juursema <github@jonathanj.nl>
2017-11-12 16:55:35 +0100

    Bugfix in membership fee summary. (Fixes #557)

--- resources/views/emails/fee.blade.php
+++ resources/views/emails/fee.blade.php
@@ -10,33 +10,33 @@
         I just ran the membership fee program. The following people had their membership fee charged.
     </p>
 
-    @if (count($charged_fees['regular']) > 0)
+    @if (count($charged_fees->regular) > 0)
         <p>
             <strong>Creative Technology students paying the regular fee</strong>
         <ul>
-            @foreach($charged_fees['regular'] as $user)
+            @foreach($charged_fees->regular as $user)
                 <li>{{ $user }}</li>
             @endforeach
         </ul>
         </p>
     @endif
 
-    @if (count($charged_fees['reduced']) > 0)
+    @if (count($charged_fees->reduced) > 0)
         <p>
             <strong>Other people paying the reduced fee</strong>
         <ul>
-            @foreach($charged_fees['reduced'] as $user)
+            @foreach($charged_fees->reduced as $user)
                 <li>{{ $user }}</li>
             @endforeach
         </ul>
         </p>
     @endif
 
-    @if (count($charged_fees['remitted']) > 0)
+    @if (count($charged_fees->remitted) > 0)
         <p>
             <strong>Exceptions who don't pay membership fee, resolve manually as needed</strong>
         <ul>
-            @foreach($charged_fees['remitted'] as $user)
+            @foreach($charged_fees->remitted as $user)
                 <li>{{ $user }}</li>
             @endforeach
         </ul>
