6e23cbdbd7be5a83af13fef982f168d8874f2eb4
Jonathan Juursema <github@jonathanj.nl>
2017-11-28 22:03:21 +0100

    Get previous and next photo from photo in album.

--- app/Models/FlickrItem.php
+++ app/Models/FlickrItem.php
@@ -4,6 +4,8 @@ namespace Proto\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
 class FlickrItem extends Model
 {
     protected $table = 'flickr_items';
@@ -15,5 +17,34 @@ class FlickrItem extends Model
     public function album()
     {
         return $this->belongsTo('Proto\Models\FlickrAlbum', 'album_id');
+    }
+
+    private function getAdjacentPhoto($next = true)
+    {
+        if ($next) {
+            $func = 'MIN';
+            $comp = '>';
+        } else {
+            $func = 'MAX';
+            $comp = '<';
+        }
+
+        $result = DB::select(DB::raw(sprintf("SELECT id FROM flickr_items WHERE album_id = %s AND date_taken = (SELECT %s(date_taken) FROM flickr_items WHERE date_taken %s %s)", $this->album_id, $func, $comp, $this->date_taken)));
+        if (count($result) > 0) {
+            $id = $result[0]->id;
+            return FlickrItem::where('id', $id)->first();
+        } else {
+            return null;
+        }
+    }
+
+    public function getNextPhoto()
+    {
+        return $this->getAdjacentPhoto();
+    }
+
+    public function getPreviousPhoto()
+    {
+        return $this->getAdjacentPhoto(false);
     }
 }
