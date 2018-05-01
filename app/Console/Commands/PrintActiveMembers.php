bc3259afb710a1ed9bff5401575c0b972a96b597
Jonathan Juursema <github@jonathanj.nl>
2018-05-01 20:44:36 +0200

    Console command to print overview of active members. (Fixes #714)

---
+++ app/Console/Commands/PrintActiveMembers.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Proto\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use Proto\Models\Member;
+
+class PrintActiveMembers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'proto:printactivemembers';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Print all active members of the association and the committees they are currently in.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        foreach (Member::all() as $member) {
+            if (!$member->user->isActiveMember()) {
+                continue;
+            }
+            $this->info(sprintf("%s: %s", $member->user->name, implode(", ", $member->user->committees->pluck("name")->toArray())));
+        }
+    }
+}
