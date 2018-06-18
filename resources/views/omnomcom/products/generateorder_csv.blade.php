63c5aa6601a9873e0fa21f89eeb2c8875c2ab04a
Jonathan Juursema <github@jonathanj.nl>
2018-06-18 13:09:19 +0200

    Added extra enter to Makro export. (Fixes #754)

--- resources/views/omnomcom/products/generateorder_csv.blade.php
+++ resources/views/omnomcom/products/generateorder_csv.blade.php
@@ -1,3 +1,3 @@
 @foreach($orders as $order)
-{{ $order->product->supplier_id or 'NO_SUPPLIER_ID' }};{{ $order->order_collo }};OmNomCom Auto Order - {{ $order->product->name }}
+{{ $order->product->supplier_id or 'NO_SUPPLIER_ID' }};{{ $order->order_collo }};OmNomCom Auto Order - {{ $order->product->name }}&#13;&#10;
 @endforeach
