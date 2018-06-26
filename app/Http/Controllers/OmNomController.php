1c605d88c493c1f9b638cafae7884d90b0952204
Jonathan Juursema <jonathanjuursema@users.noreply.github.com>
2018-06-26 22:08:51 +0200

    Bugfix in daily OmNomCom total.

--- app/Http/Controllers/OmNomController.php
+++ app/Http/Controllers/OmNomController.php
@@ -145,7 +145,7 @@ class OmNomController extends Controller
         }
 
         if ($user->show_omnomcom_total) {
-            return sprintf("OK,TOTAL2,%s,%s", $user->calling_name, OrderLine::where('user_id', $user->id)->where('created_at', 'LIKE', sprintf("%s %%", date('Y-m-d')))->sum('total_price'));
+            return sprintf("OK,TOTAL,%s,%s", $user->calling_name, OrderLine::where('user_id', $user->id)->where('created_at', 'LIKE', sprintf("%s %%", date('Y-m-d')))->sum('total_price'));
         } else {
             return "OK";
         }
