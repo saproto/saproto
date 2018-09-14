9fc1638ef5dba64ccf4d640cbf0850f3474e2cdd
Thijs Dortmann <thijs@thijsdortmann.nl>
2018-09-14 11:08:00 +0200

    Fix TFA not working anymore after dashboard no longer requiring user ID in URL

--- app/Http/Middleware/EnforceTFA.php
+++ app/Http/Middleware/EnforceTFA.php
@@ -19,7 +19,7 @@ class EnforceTFA
     public function handle($request, Closure $next)
     {
         if (App::environment('production') && Auth::check() && Auth::user()->hasRole(config('proto.tfaroles')) && (!Auth::user()->hasTFAEnabled())) {
-            if (!$request->is('user/dashboard') && !$request->is('auth/logout') && !$request->is('user/quit_impersonating') && !$request->is('user/*/2fa/*') && !$request->is('api/*')) {
+            if (!$request->is('user/dashboard') && !$request->is('auth/logout') && !$request->is('user/quit_impersonating') && !$request->is('user/*/2fa/*') && !$request->is('user/2fa/*') && !$request->is('api/*')) {
                 $request->session()->flash('flash_message', 'Your account permissions require you to enable Two Factor Authentication on your account before being able to use your account.');
                 return Redirect::route('user::dashboard', array('#2fa'));
             }
