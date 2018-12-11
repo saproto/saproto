0f96fed5b9addb1a3a931d557a2ecdc0b8abf951
Jonathan Juursema <github@jonathanj.nl>
2018-12-11 21:45:14 +0100

    One more attempt at newlines in OmNomCom CSV export. (Fixes #754)

--- resources/views/omnomcom/products/generateorder_csv.blade.php
+++ resources/views/omnomcom/products/generateorder_csv.blade.php
@@ -1,3 +1,5 @@
+<pre>
 @foreach($orders as $order)
-{{ $order->product->supplier_id or 'NO_SUPPLIER_ID' }};{{ $order->order_collo }};OmNomCom Auto Order - {{ $order->product->name }}&#13;&#10;
+{{ $order->product->supplier_id or 'NO_SUPPLIER_ID' }};{{ $order->order_collo }};OmNomCom Auto Order - {{ $order->product->name }}
 @endforeach
+</pre>
