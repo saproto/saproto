51908ae305733318484e917d11b56b6305c2afb4
thijsdortmann <thijs@thijsdortmann.nl>
2019-01-22 18:16:17 +0100

    Fix #888.

--- resources/views/protube/remote.blade.php
+++ resources/views/protube/remote.blade.php
@@ -561,7 +561,7 @@
             <div class="col-md-8 col-12 justify-content-center">
 
                 <form id="protube__remote__ytSearch_form"><input type="search" id="protube__remote__ytSearch"
-                                                                 class="form-control" placeholder="Search..."
+                                                                 class="form-control" placeholder="Search on Youtube (maximum duration <?php echo(\Carbon\CarbonInterval::seconds(file_get_contents(config('herbert.server') . "/maxDuration?secret=" . config('herbert.secret')))->cascade()->forHumans()); ?>)..."
                                                                  style="width: 100%;">
                 </form>
 
@@ -596,7 +596,6 @@
 
     <div class="container" id="protube__remote__searchResults">
         <div class="row" id="protube__remote__searchResults_row">
-
         </div>
     </div>
 
