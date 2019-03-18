1418d12e411146c5a2c470662ab5d293a9e0df65
jessseee <j.w.visser-3@student.utwente.nl>
2019-03-18 15:13:47 +0100

    added use_dark_theme to user controller and fixed stylesheet selection

--- resources/views/website/layouts/assets/stylesheets.blade.php
+++ resources/views/website/layouts/assets/stylesheets.blade.php
@@ -1,4 +1,4 @@
-@if(Auth::check() && $user->use_dark_theme)
+@if(Auth::check() && Auth::user()->use_dark_theme)
     <link rel="stylesheet" href="{{ asset('assets/application-dark.min.css') }}">
 @else
     <link rel="stylesheet" href="{{ asset('assets/application-light.min.css') }}">
