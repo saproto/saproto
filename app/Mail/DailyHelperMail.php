28db7e372166904d116d07efda13d54602b16047
thijsdortmann <thijs@thijsdortmann.nl>
2019-04-02 11:47:28 +0200

    Fix typo in class name.

--- app/Mail/DailyHelperMail.php
+++ app/Mail/DailyHelperMail.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class DailyHelperMaild extends Mailable
+class DailyHelperMail extends Mailable
 {
     use Queueable, SerializesModels;
 
