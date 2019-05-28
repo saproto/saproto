3703573c7ead77cb762f59f6e40438f07875064a
Jonathan Juursema <github@jonathanj.nl>
2019-05-28 22:29:14 +0200

    Better sort for DMX overrides. (Fixes #961)

---
+++ resources/views/dmx/override/override_macro.blade.php
@@ -0,0 +1,45 @@
+<tr style="{{ $override->active() ? '' : 'opacity: 0.5;' }}">
+
+    <td>
+        @foreach($override->getFixtures() as $fixture)
+            {{ $fixture->name }}<br>
+        @endforeach
+    </td>
+    <td>
+                                    <span style="color: red;">
+                                        <i class="fas fa-tint" aria-hidden="true"></i>
+                                        {{ $override->red() }}
+                                    </span>
+    </td>
+    <td>
+                                    <span style="color: green;">
+                                        <i class="fas fa-tint" aria-hidden="true"></i>
+                                        {{ $override->green() }}
+                                    </span>
+    </td>
+    <td>
+                                    <span style="color: blue;">
+                                        <i class="fas fa-tint" aria-hidden="true"></i>
+                                        {{ $override->blue() }}
+                                    </span>
+    </td>
+    <td>
+        <i class="fas fa-sun" aria-hidden="true"></i>
+        {{ $override->brightness() }}
+    </td>
+    <td>
+        Start: {{ date('l F j Y, H:i', $override->start) }}<br>
+        End: {{ date('l F j Y, H:i', $override->end) }}
+    </td>
+    <td>
+        <a class="btn btn-xs btn-default mr-2"
+           href="{{ route('dmx::override::edit', ['id' => $override->id]) }}">
+            <i class="fas fa-edit"></i>
+        </a>
+        <a class="btn btn-xs btn-danger"
+           href="{{ route('dmx::override::delete', ['id' => $override->id]) }}">
+            <i class="fas fa-trash"></i>
+        </a>
+    </td>
+
+</tr>
