815667f14f491211e07188b682c12eaa6792a6fe
Jonathan Juursema <github@jonathanj.nl>
2019-11-23 23:11:53 +0100

    Added birthday photo. (Fixes #1028)

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,18 +2,14 @@
 
 namespace Proto\Models;
 
-use Proto\Models\HashMapItem;
-use Proto\Models\WelcomeMessage;
 
 use Adldap\Adldap;
 use Adldap\Connections\Provider;
-use Adldap\Objects\AccountControl;
 
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
--- resources/views/emails/users/birthdayemail.blade.php
+++ resources/views/emails/users/birthdayemail.blade.php
@@ -7,12 +7,13 @@
     </p>
 
     <p>
-        I am writing you to congratulate you with
-        your {{ (new NumberFormatter('en_US', NumberFormatter::ORDINAL))->format($user->age()) }} birthday! If you are
+        I am writing you to congratulate you with your birthday! If you are
         able to drop by the Protopolis today I would love to congratulate you in person and offer you a free and
         complimentary birthday cookie. Is there a Proto drink on your birthday? You can also choose to get
         a free birthday pull during the drink. Have a great day!
     </p>
+
+    <img src="{{ asset('images/emails/birthday.jpg') }}" style="width: 100%;">
 
     <p>
         Kind regards,
