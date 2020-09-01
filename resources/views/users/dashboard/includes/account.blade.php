ee19c90093e837334a24ed9cdb805754e530c76c
Jonathan Juursema <jonathan.juursema@topicus.nl>
2020-09-01 22:28:55 +0200

    Added security improvements.

--- resources/views/users/dashboard/includes/account.blade.php
+++ resources/views/users/dashboard/includes/account.blade.php
@@ -1,3 +1,58 @@
+<form class="form-horizontal" method="post" action="{{ route("user::changemail") }}">
+
+    {!! csrf_field() !!}
+
+    <div class="card mb-3">
+
+        <div class="card-header bg-dark text-white">
+            Your e-mail address
+        </div>
+
+        <div class="card-body">
+
+            <p>
+                Here you can change your e-mail address. You'll receive an e-mail on both the old and new address for
+                confirmation. You need your password to change your e-mail address.
+            </p>
+
+            <table class="table table-borderless table-sm mb-0">
+
+                <tbody>
+
+                <tr>
+                    <th>E-mail</th>
+                    <td>
+                        <input type="email" class="form-control form-control-sm" id="email" name="email"
+                               value="{{ $user->email }}" required>
+                    </td>
+                </tr>
+
+                <tr>
+                    <th>Current password</th>
+                    <td>
+                        <input type="password" class="form-control form-control-sm" id="password" name="password"
+                               required>
+                    </td>
+                </tr>
+
+                </tbody>
+
+            </table>
+
+        </div>
+
+        <div class="card-footer">
+
+            <button type="submit" class="btn btn-outline-info btn-block">
+                Change your e-mail
+            </button>
+
+        </div>
+
+    </div>
+
+</form>
+
 <form class="form-horizontal" method="post" action="{{ route("user::dashboard") }}">
 
     {!! csrf_field() !!}
@@ -5,7 +60,7 @@
     <div class="card mb-3">
 
         <div class="card-header bg-dark text-white">
-            Your account
+            Your personal details
         </div>
 
         <div class="card-body">
@@ -17,14 +72,6 @@
                 <tr>
                     <th>Name</th>
                     <td>{{ $user->name }} ({{{ $user->calling_name }}})</td>
-                </tr>
-
-                <tr>
-                    <th>E-mail</th>
-                    <td>
-                        <input type="email" class="form-control form-control-sm" id="email" name="email"
-                               value="{{ $user->email }}" required>
-                    </td>
                 </tr>
 
                 @if($user->did_study_create || $user->did_study_itech)
