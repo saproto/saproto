1be9188cd7142d04b24e8dbfebe91352a4bfcc71
Max Liebe <amalox@outlook.com>
2020-12-22 15:00:25 +0100

    Update LeaderboardController.php

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -6,6 +6,9 @@ namespace Proto\Http\Controllers;
 use Illuminate\Http\Request;
 use Proto\Models\Leaderboard;
 
+use Session;
+use Redirect;
+
 class LeaderboardController extends Controller
 {
     /**
@@ -13,14 +16,51 @@ class LeaderboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function adminIndex()
     {
         $leaderboards = Leaderboard::get();
         if (count($leaderboards) > 0) {
-            return view('leaderboards.list', ['leaderboards' => $leaderboards]);
+            return view('leaderboards.adminlist', ['leaderboards' => $leaderboards]);
         } else {
             Session::flash("flash_message", "There is currently nothing to see on the leaderboards page, but please check back real soon!");
             return Redirect::back();
         }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('leaderboards.edit', ['leaderboard' => null]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $leaderboard = Leaderboard::create($request->all());
+        $leaderboard->save();
+
+        Session::flash("flash_message", "Your leaderboard '" . $leaderboard->name . "' has been added.");
+        return Redirect::route('leaderboards::admin');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $leaderboard = Leaderboard::findOrFail($id);
+        return view('leaderboards.edit', ['leaderboard' => $leaderboard]);
     }
 }
