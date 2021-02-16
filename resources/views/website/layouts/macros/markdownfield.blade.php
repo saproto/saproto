a3dc6132bb305d58327ae8a2d9eef63c3aabdba2
Jessseee <jssvssr2000@gmail.com>
2021-02-16 21:46:36 +0100

    Updated SimpleMDE to more up to date EasyMDE fork

--- resources/views/website/layouts/macros/markdownfield.blade.php
+++ resources/views/website/layouts/macros/markdownfield.blade.php
@@ -1,14 +1,18 @@
 <textarea id="markdownfield-{{ $name }}" name="{{ $name }}"
-          {!! isset($placeholder) && $placeholder !== null ? sprintf('placeholder="%s"', $placeholder) : null !!}
->{!! isset($value) && $value !== null ? $value : null !!}</textarea>
+          {!! isset($placeholder) ? sprintf('placeholder="%s"', $placeholder) : null !!}
+>{!! isset($value) ? $value : null !!}</textarea>
 
 @push('javascript')
 
     <script>
-        var simplemde = new SimpleMDE({
+        var easyMDE = new EasyMDE({
             element: $("#markdownfield-{{ $name }}")[0],
             toolbar: ["bold", "italic", "|", "unordered-list", "ordered-list", "|", "image", "link", "quote", "table", "code", "|", "preview"],
-            spellChecker: false
+        });
+
+        $('.editor-statusbar').each(function() {
+            if ($(this).find(".md-ref").length === 0)
+                $(this).prepend("<a class='md-ref float-left' target='_blank' href='https://www.markdownguide.org/basic-syntax/'>markdown syntax</a>");
         });
     </script>
 
