2d0cba5c198407f6bdaceda6e1cee91600d26151
Max Liebe <amalox@outlook.com>
2021-03-16 20:27:59 +0100

    Added option to redirect to job offer directly instead of a description (#1227)
    * Added option to redirect to job offer directly instead of a description
    
    - Table joboffers now has a new redirect_url column
    - In the Admin 'add job offer' menu there is now an option to enter a redirect url instead of a description
    - By clicking on a job offer with a redirect url, you will be redirected to that job offer page. If the job offer does not have a redirect url, it will open the job offer on the Proto site with a description just like before.
    
    * Fixed changes request by @JonaMata
    
    * Fixed changes request by @JonaMata
    
    * Updated if/else block to be more intuitive
    
    * Updated message displayed to user
    
    * Might have accidently deleted the docker env example in my first commit, might have not.

--- resources/views/companies/joboffers/edit.blade.php
+++ resources/views/companies/joboffers/edit.blade.php
@@ -42,8 +42,17 @@
                                    placeholder="Chief Executive Officer" value="{{ $joboffer->title or '' }}" required>
                         </div>
 
-
                         <div class="form-group">
+                            <label>Offer information type</label>
+                            <select id="information_type_selector" class="form-control">
+                                <option value="" @if(!$joboffer || ($joboffer->description == null && $joboffer->redirect_url == null)) selected @endif disabled>Select a type...
+                                </option>
+                                <option value="description" @if($joboffer && $joboffer->description != null) selected @endif>Description</option>
+                                <option value="url" @if($joboffer && $joboffer->redirect_url != null) selected @endif>Redirect URL</option>
+                            </select>
+                        </div>
+
+                        <div id="information_type_description" class="form-group">
                             <label for="editor-description">Description</label>
                             @include('website.layouts.macros.markdownfield', [
                                 'name' => 'description',
@@ -51,6 +60,22 @@
                                 'value' => !$joboffer ? null : $joboffer->description
                             ])
                         </div>
+
+                        <div id="information_type_url" class="form-group">
+                            <label for="title">Redirect URL</label>
+                            <input type="text" class="form-control" id="redirect_url" name="redirect_url"
+                                   placeholder="https://example.com/apply" value="{{ $joboffer->redirect_url or '' }}">
+                        </div>
+
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
 
                     </div>
 
@@ -66,5 +91,36 @@
         </div>
 
     </form>
+
+@endsection
+
+@section('javascript')
+
+    @parent
+
+    <script>
+        $(document).ready(updateInformationDisplay);
+        $('#information_type_selector').change(updateInformationDisplay);
+
+        function updateInformationDisplay() {
+            $('#information_type_description, #information_type_url').removeClass('d-none');
+            switch($('#information_type_selector').val()) {
+                case 'description':
+                    $('#information_type_url').addClass('d-none');
+                    $('#redirect_url').val('').removeAttr('required');
+                    break;
+                case 'url':
+                    $('#information_type_description').addClass('d-none');
+                    $('#redirect_url').attr('required', 'true');
+                    simplemde.value('');
+                    break;
+                default:
+                    $('#information_type_url, #information_type_description').addClass('d-none').find('input').val('');
+                    $('#information_type_selector').attr('required', 'true');
+                    simplemde.value('');
+                    break;
+            }
+        }
+    </script>
 
 @endsection
