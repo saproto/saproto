255cf1d67f6f3b53f8a108839abbb1fa0962273e
Jessseee <jssvssr2000@gmail.com>
2021-04-03 10:44:39 +0200

    Fixed login/register pages scrolling horizontally

--- resources/views/auth/template.blade.php
+++ resources/views/auth/template.blade.php
@@ -2,29 +2,33 @@
 
 @section('body')
 
-    <div class="row justify-content-center">
+    <div class="container">
 
-        <div class="col-xl-3 col-lg-5 col-md-6 col-sm-10 col-xs-12 mx-3 text-center">
+        <div class="row justify-content-center">
 
-            <div class="card mb-3 mt-5">
+            <div class="col-12 col-lg-5 text-center">
 
-                <div class="card-header text-center bg-dark text-white">
-                    S.A. Proto | @yield('page-title')
+                <div class="card mb-3 mt-5">
+
+                    <div class="card-header text-center bg-dark text-white">
+                        S.A. Proto | @yield('page-title')
+                    </div>
+
+                    <div class="card-body text-center">
+
+                        @yield('login-body')
+
+                    </div>
+
+                    <a href="/" style="text-decoration: none !important;" class="card-footer text-muted text-center">
+                        Go back to homepage.
+                    </a>
+
                 </div>
 
-                <div class="card-body text-center">
-
-                    @yield('login-body')
-
-                </div>
-
-                <a href="/" style="text-decoration: none !important;" class="card-footer text-muted text-center">
-                    Go back to homepage.
-                </a>
+                <img src="{{ asset('images/logo/regular.png') }}" width="60%" class="mb-3 mt-5" alt="Proto logo">
 
             </div>
-
-            <img src="{{ asset('images/logo/regular.png') }}" width="60%" class="mb-3 mt-5">
 
         </div>
 
