76101d08a5e80960f9299b8145b7b9db746e15b0
Jessseee <jssvssr2000@gmail.com>
2021-04-05 19:05:16 +0200

    Fixed leaderboard description not rendering Markdown

--- resources/views/leaderboards/list.blade.php
+++ resources/views/leaderboards/list.blade.php
@@ -21,7 +21,7 @@
                          class="table-responsive collapse {{ $loop->index == 0 ? 'show' : '' }}"
                          data-parent="#leaderboard-accordion">
                         @if($leaderboard->description)
-                            <p class="px-3 pt-3">{{ $leaderboard->description }}</p>
+                            <div class="px-3 pt-3">{!! Markdown::convertToHtml($leaderboard->description) !!}</div>
                         @endif
                         @if(count($leaderboard->entries) > 0)
                             <table class="table table-sm">
