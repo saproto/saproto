5aa5f08d494d966b61c0405165fec6a46e85b803
Jessseee <jssvssr2000@gmail.com>
2021-04-23 11:54:48 +0200

    Updated back-end dependencies and Sentry config

--- config/sentry.php
+++ config/sentry.php
@@ -1,11 +1,31 @@
 <?php
 
-return array(
-    'dsn' => env('SENTRY_DSN'),
-    
-    // Capture bindings on SQL queries
-    'breadcrumbs.sql_bindings' => true,
+return [
+    'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
-    // Capture default user context
-    'user_context' => false,
-);
+    // capture release as git sha
+    'release' => trim(exec('git --git-dir ' . base_path('.git') . ' log --pretty="%h" -n1 HEAD')),
+
+    // When left empty or `null` the Laravel environment will be used
+    'environment' => null,
+
+    'breadcrumbs' => [
+        // Capture Laravel logs in breadcrumbs
+        'logs' => true,
+
+        // Capture SQL queries in breadcrumbs
+        'sql_queries' => true,
+
+        // Capture bindings on SQL queries logged in breadcrumbs
+        'sql_bindings' => true,
+
+        // Capture queue job information in breadcrumbs
+        'queue_info' => true,
+
+        // Capture command information in breadcrumbs
+        'command_info' => true,
+    ],
+
+    // @see: https://docs.sentry.io/platforms/php/configuration/options/#send-default-pii
+    'send_default_pii' => false,
+];
