7ffe7357ce00217a71e0e5de9ddcd02cf906a252
Jessseee <jssvssr2000@gmail.com>
2021-05-26 14:55:30 +0200

    Removed non-existing Slack routes from javascript

--- resources/views/website/layouts/assets/javascripts.blade.php
+++ resources/views/website/layouts/assets/javascripts.blade.php
@@ -34,10 +34,6 @@
             }, 500);
         });
 
-        @if (Auth::check() && Auth::user()->is_member)
-            initSlack('{{ route('api::slack::count') }}', '{{ route('api::slack::invite') }}');
-        @endif
-
         @if (Auth::check())
             initDiscord();
         @endif
