9691ace69452aa8f6afe5e502b14743f77f850a2
Jessseee <jssvssr2000@gmail.com>
2021-08-26 22:23:41 +0900

    Fixed sentry releases

--- config/sentry.php
+++ config/sentry.php
@@ -4,7 +4,7 @@ return [
     'dsn' => env('SENTRY_LARAVEL_DSN', env('SENTRY_DSN')),
 
     // capture release as git sha
-    'release' => trim(exec('git --git-dir '.base_path('.git').' log --pretty="%h" -n1 HEAD')),
+    'release' => 'c9d0c53a',
 
     // When left empty or `null` the Laravel environment will be used
     'environment' => null,
