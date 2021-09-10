2cf8e116a040fa45fcc54c2a30ce71af922289ec
Jonathan Matarazzi <git@jonathanm.nl>
2021-09-10 13:48:53 +0200

    Limited executable permission to file owner

--- deploy.sh
+++ deploy.sh
@@ -29,8 +29,8 @@ echo "Shuffling files..."
 cp uploads/checksum.txt new_build
 cp environment/.env new_build
 echo "Fixing file permissions"
-chmod +x new_build/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64
-chmod +x new_build/vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64
+chmod u+x new_build/vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64
+chmod u+x new_build/vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64
 echo "Symlinking storage..."
 ln -s /actual/path \
   /symlinked/path
