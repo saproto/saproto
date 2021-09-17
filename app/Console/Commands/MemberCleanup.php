b0ee1eff0a0213a7f87d7d88bad12ad123e1e598
Jonathan Matarazzi <git@jonathanm.nl>
2021-09-17 22:19:48 +0200

    Added pending member cleanup cron

---
+++ app/Console/Commands/MemberCleanup.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Proto\Console\Commands;
+
+use Carbon\Carbon;
+use Exception;
+use Illuminate\Console\Command;
+use Proto\Models\Member;
+
+class MemberCleanup extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'proto:membercleanup}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Delete all pending memberships older than a month.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $old_pending_memberships = Member::where('is_pending', true)->where('created_at', '<', Carbon::now()->subMonth(1))->get();
+        foreach ($old_pending_memberships as $pending_membership) {
+            $pending_membership->delete();
+        }
+        return 0;
+    }
+}
