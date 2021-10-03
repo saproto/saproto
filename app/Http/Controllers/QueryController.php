c521b9e54ee2eee44ac094997a4067a2810f5258
Jonathan Matarazzi <git@jonathanm.nl>
2021-10-03 16:50:42 +0200

    Count pending members seperately in membership totals (#1358)
    * Count pending members seperately in membership totals
    
    * Apply php-cs-fixer changes
    
    Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>

--- app/Http/Controllers/QueryController.php
+++ app/Http/Controllers/QueryController.php
@@ -76,6 +76,7 @@ class QueryController extends Controller
         $count_lifelong = 0;
         $count_honorary = 0;
         $count_donor = 0;
+        $count_pending = 0;
 
         $export_subsidies = [];
         $export_active = [];
@@ -88,48 +89,52 @@ class QueryController extends Controller
             $has_ut_mail = substr($member->user->email, -10) == 'utwente.nl';
             $is_ut = $is_primary_student || $has_ut_mail || $member->user->utwente_username !== null;
 
-            if (! $member->is_pet) {
-                $count_total++;
-            }
-
-            if ($member->user->isActiveMember()) {
-                $count_active++;
-
-                if ($request->has('export_active')) {
-                    $export_active[] = (object) [
-                        'name' => $member->user->name,
-                        'committees' => $member->user->committees->pluck('name'),
-                    ];
-                }
-            }
-            if ($member->is_lifelong) {
-                $count_lifelong++;
-            }
-            if ($member->is_honorary) {
-                $count_honorary++;
-            }
-            if ($member->is_donor) {
-                $count_donor++;
-            }
-
-            if ($is_primary_student) {
-                $count_primary++;
+            if ($member->is_pending) {
+                $count_pending++;
             } else {
-                $count_secondary++;
-            }
+                if (! $member->is_pet) {
+                    $count_total++;
+                }
 
-            if ($is_ut) {
-                $count_ut++;
-            }
+                if ($member->user->isActiveMember()) {
+                    $count_active++;
 
-            if ($request->has('export_subsidies')) {
+                    if ($request->has('export_active')) {
+                        $export_active[] = (object) [
+                            'name' => $member->user->name,
+                            'committees' => $member->user->committees->pluck('name'),
+                        ];
+                    }
+                }
+                if ($member->is_lifelong) {
+                    $count_lifelong++;
+                }
+                if ($member->is_honorary) {
+                    $count_honorary++;
+                }
+                if ($member->is_donor) {
+                    $count_donor++;
+                }
+
+                if ($is_primary_student) {
+                    $count_primary++;
+                } else {
+                    $count_secondary++;
+                }
+
                 if ($is_ut) {
-                    $export_subsidies[] = (object) [
-                        'primary' => $is_primary_student ? 'true' : 'false',
-                        'name' => $member->user->name,
-                        'email' => $has_ut_mail ? $member->user->email : null,
-                        'ut_number' => $member->user->utwente_username ? $member->user->utwente_username : null,
-                    ];
+                    $count_ut++;
+                }
+
+                if ($request->has('export_subsidies')) {
+                    if ($is_ut) {
+                        $export_subsidies[] = (object) [
+                            'primary' => $is_primary_student ? 'true' : 'false',
+                            'name' => $member->user->name,
+                            'email' => $has_ut_mail ? $member->user->email : null,
+                            'ut_number' => $member->user->utwente_username ? $member->user->utwente_username : null,
+                        ];
+                    }
                 }
             }
         }
@@ -160,6 +165,7 @@ class QueryController extends Controller
                 'lifelong' => $count_lifelong,
                 'honorary' => $count_honorary,
                 'donor' => $count_donor,
+                'pending' => $count_pending,
             ]);
         }
     }
