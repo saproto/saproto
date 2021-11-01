f0bb02a3abf7677c95f539b4d86a3c893b855a80
Jonathan Matarazzi <git@jonathanm.nl>
2021-11-01 14:19:56 +0100

    Remove 2 categories from tipcie store

--- config/omnomcom.php
+++ config/omnomcom.php
@@ -24,7 +24,7 @@ return [
         ],
         'tipcie' => (object) [
             'name' => 'TIPCie',
-            'categories' => [15, 18, 25, 29, 30],
+            'categories' => [15, 18, 25],
             'addresses' => [],
             'roles' => ['board', 'tipcie', 'drafters'],
             'cash_allowed' => false,
