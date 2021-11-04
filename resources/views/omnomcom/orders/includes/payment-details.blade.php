364e43dfd62f969d75c6eb88bc21ccc945b5a41c
Jonathan Matarazzi <git@jonathanm.nl>
2021-11-05 00:44:35 +0100

    Revert mollie block

--- resources/views/omnomcom/orders/includes/payment-details.blade.php
+++ resources/views/omnomcom/orders/includes/payment-details.blade.php
@@ -42,8 +42,7 @@
 
         <div class="card-footer">
 
-            {{--    Disabled because of bug with mollie payments https://github.com/saproto/saproto/issues/1367   --}}
-            <a href="javascript:void();" class="btn btn-secondary btn-block disabled" data-toggle="modal" data-target="#mollie-modal">
+            <a href="javascript:void();" class="btn btn-primary btn-block" data-toggle="modal" data-target="#mollie-modal">
                 Pay now
             </a>
 
