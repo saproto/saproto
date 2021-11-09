4303acf697c546d8fceb4e78f91b8f4c0cb4ee13
Jonathan Matarazzi <git@jonathanm.nl>
2021-11-09 19:57:24 +0100

    Add popup after new achievement has been awarded (#1377)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Proto\Providers;
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Proto\Models\MenuItem;
 
@@ -17,6 +18,14 @@ class AppServiceProvider extends ServiceProvider
         view()->composer('website.navigation.navbar', function ($view) {
             $menuItems = MenuItem::where('parent', null)->orderBy('order')->get();
             $view->with('menuItems', $menuItems);
+        });
+        view()->composer('website.layouts.macros.achievement-popup', function ($view) {
+            if (Auth::check()) {
+                $newAchievementsQuery = Auth::user()->achievements()->where('alerted', false);
+                $newAchievements = $newAchievementsQuery->get();
+                $newAchievementsQuery->update(['alerted' => true]);
+                $view->with('newAchievements', $newAchievements);
+            }
         });
     }
 
