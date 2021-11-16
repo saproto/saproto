a891bdd84b77fe26f494d3c2a64725e64461bc3b
ysbrandB <ysbrand@burgstede.eu>
2021-11-16 20:41:50 +0100

    Disable_Mollie
    Disables mollie until its fixed, disabled because of issue 1414

--- resources/views/omnomcom/orders/includes/payment-details.blade.php
+++ resources/views/omnomcom/orders/includes/payment-details.blade.php
@@ -41,11 +41,11 @@
     @if($next_withdrawal > 0)
 
         <div class="card-footer">
-
+            <!-- Disabled because of mollie issues and so users dont get charged the mollie fee, see: https://github.com/saproto/saproto/issues/1414
             <a href="javascript:void();" class="btn btn-primary btn-block" data-toggle="modal" data-target="#mollie-modal">
                 Pay now
             </a>
-
+            -->
         </div>
 
     @elseif($next_withdrawal < 0)
