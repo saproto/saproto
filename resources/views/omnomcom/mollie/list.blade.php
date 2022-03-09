815977814deb70d1d5b289d4cea11b7892812ec4
Menkveld-24 <menke.veerman@gmail.com>
2022-03-09 19:46:36 +0100

    Removed redundant code

--- resources/views/omnomcom/mollie/list.blade.php
+++ resources/views/omnomcom/mollie/list.blade.php
@@ -84,16 +84,6 @@
                                     {!! Proto\Models\MollieTransaction::translateStatus($transaction->translatedStatus()) == "paid" ? '<i class="fas fa-check ml-2 text-success"></i>' : "" !!}
                                     {!! Proto\Models\MollieTransaction::translateStatus($transaction->translatedStatus()) == "unknown" ? '<i class="fas fa-question ml-2 text-normal"></i>' : "" !!}
                                     <span class="label label-default"> - {{ $transaction->status }}</span>
-                                    {{-- Doen de labels wel iets?? --}}
-                                    {{-- @if(Proto\Models\MollieTransaction::translateStatus($transaction->status) == 'open')
-                                        <span class="label label-default">{{ $transaction->status }}</span>
-                                    @elseif(Proto\Models\MollieTransaction::translateStatus($transaction->status) == 'paid')
-                                        <span class="label label-success">{{ $transaction->status }}</span>
-                                    @elseif(Proto\Models\MollieTransaction::translateStatus($transaction->status) == 'failed')
-                                        <span class="label label-danger">{{ $transaction->status }}</span>
-                                    @else
-                                        <span class="label label-warning">{{ $transaction->status }}</span>
-                                    @endif --}}
                                 </td>
 
                                 <td>
