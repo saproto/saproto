8acd3c2ee72ad85f35c275a6b26dc609e33b5dc7
Jessseee <jssvssr2000@gmail.com>
2022-04-03 18:35:13 +0200

    Fixed achievement modal

--- resources/views/website/layouts/macros/achievement-popup.blade.php
+++ resources/views/website/layouts/macros/achievement-popup.blade.php
@@ -1,6 +1,6 @@
 @if(isset($newAchievements) && count($newAchievements)>0)
 
-    <div class="modal fade" id="newAchievementModal" tabindex="-1" role="dialog"
+    <div class="modal fade" id="new-achievement-modal" tabindex="-1" role="dialog"
          aria-labelledby="newAchievementModalLabel">
         <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content bg-dark text-white">
@@ -17,6 +17,6 @@
     </div>
 
     <script type="text/javascript" nonce="{{ csp_nonce() }}">
-        $('#newAchievementModal').modal('show');
+        modals['new-achievement-modal'].show()
     </script>
 @endif
