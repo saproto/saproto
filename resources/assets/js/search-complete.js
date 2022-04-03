40b6e67b14ae804a6ed68e20de99083ae4ea5162
Jessseee <jssvssr2000@gmail.com>
2022-04-03 15:55:47 +0200

    Improved readability of search-autocomplete.js

--- resources/assets/js/search-complete.js
+++ resources/assets/js/search-complete.js
@@ -7,106 +7,108 @@ export default class SearchComplete {
         this.sorter = sorter
         this.multiple = el.hasAttribute('multiple')
 
-        // Search result container
-        this.searchResults = document.createElement('div')
-        this.searchResults.className = 'search-results form-control p-0'
-        this.searchResults.id = el.id + '-search-results'
+        this.resultsContainer = this.createResultsContainer(el.id)
+        this.inputElement = this.createInputElement(el.name, el.value)
+        this.selectedContainer = this.createSelectedContainer(el.id)
+        this.selectedElement = this.createSelectedElement()
 
-        // Input template
-        this.input = document.createElement('input')
-        this.input.type = 'hidden'
-        this.input.name = el.name
-        this.input.value = el.value
         el.name = ''
         el.value = el.placeholder
 
-        // Selected item container
-        this.selected = document.createElement('div')
-        this.selected.className = 'selected-items form-control border-top-0 p-1 d-none'
-
-        // Selected item template
-        this.selectedItem = document.createElement('span')
-        this.selectedItem.className = 'selected-item d-block-inline badge bg-success ps-1 my-1 mx-2'
-
-        // Append results and hidden inputs
-        el.parentNode.append(this.searchResults)
-        if (this.multiple) el.parentNode.append(this.selected)
-        else el.parentNode.append(this.input)
+        el.parentNode.append(this.resultsContainer)
+        if (this.multiple) el.parentNode.append(this.selectedContainer)
+        else el.parentNode.append(this.inputElement)
 
         this.search()
-        el.addEventListener('keyup', this.search)
+        el.addEventListener('keyup', this.search.bind(this))
     }
 
-    search = _ => {
-        this.searchResults.innerHTML = ''
+    createInputElement(name, value) {
+        let input = document.createElement('input')
+        input.type = 'hidden'
+        input.name = name
+        input.value = value
+        return input
+    }
 
-        // Search input must be at least 3 characters
-        if (this.el.value.length < 3) return this.searchResults.innerHTML = '<span>type at least 3 characters</span>'
-        else this.searchResults.innerHTML = '<span>searching...</span>'
+    createResultsContainer(id) {
+        let resultsContainer = document.createElement('div')
+        resultsContainer.className = 'search-results form-control p-0'
+        resultsContainer.id = id + '-search-results'
+        return resultsContainer
+    }
 
-        // Get search results
+    createSelectedContainer(id) {
+        let selectedContainer = document.createElement('div')
+        selectedContainer.className = 'selected-items form-control border-top-0 p-1 d-none'
+        selectedContainer.id = id + '-selected-items'
+        return selectedContainer
+    }
+
+    createSelectedElement() {
+        let selectedElement = document.createElement('span')
+        selectedElement.className = 'selected-item d-block-inline badge bg-success ps-1 my-1 mx-2'
+        return selectedElement
+    }
+
+    createOptionElement(item) {
+        let option = document.createElement('option')
+        if (typeof this.optionTemplate === 'function') this.optionTemplate(option, item)
+        else option.innerHTML = item.name
+        option.addEventListener('click', _ => {
+            if (this.multiple) this.appendSelected(item)
+            else this.setSelected(item)
+            this.el.dispatchEvent(new Event('keyup'))
+        })
+        this.resultsContainer.append(option)
+    }
+
+    setSelected(item) {
+        this.inputElement.value = item.id
+        this.el.value = this.selectedTemplate?.(item) ?? item.name ?? item.id
+        this.resultsContainer.innerHTML = ''
+        this.resultsContainer.append(option)
+    }
+
+    appendSelected(item) {
+        this.selectedContainer.classList.remove('d-none')
+        this.el.required = false
+
+        let newSelectedItem = this.selectedElement.cloneNode()
+        newSelectedItem.innerHTML = this.selectedTemplate?.(item) ?? item.id
+        newSelectedItem.addEventListener('click', _ => {
+            newSelectedItem.remove()
+            if (this.selectedContainer.children.length === 0) {
+                this.el.required = true
+                this.selectedContainer.classList.add('d-none')
+            }
+        })
+
+        let multipleInput = this.inputElement.cloneNode()
+        multipleInput.value = item.id
+
+        newSelectedItem.append(multipleInput)
+        this.selectedContainer.append(newSelectedItem)
+
+        this.el.value = ''
+        this.resultsContainer.innerHTML = ''
+    }
+
+    search()  {
+        this.resultsContainer.innerHTML = ''
+
+        if (this.el.value.length < 3) return this.resultsContainer.innerHTML = '<span>type at least 3 characters</span>'
+        else this.resultsContainer.innerHTML = '<span>searching...</span>'
+
         get( this.route,{q: this.el.value})
         .then(data => {
-
-            // Check if there are any results
-            if (data.length === 0) return this.searchResults.innerHTML = '<span>no results</span>'
-            else this.searchResults.innerHTML = ''
-
-            // Sort data if sorter is defined
+            if (data.length === 0) return this.resultsContainer.innerHTML = '<span>no results</span>'
+            else this.resultsContainer.innerHTML = ''
             if (this.sorter === 'function') data.sort(this.sorter)
-
-            data.forEach(item => {
-                // For each result create an option in the search results container
-                let option = document.createElement('option')
-                if (typeof this.optionTemplate === 'function') this.optionTemplate(option, item)
-                else option.innerHTML = item.name
-
-                // Add onclick eventListener to option to be able to select one
-                option.addEventListener('click', _ => {
-                    // Check if input accepts multiple selections
-                    if (this.multiple) {
-                        this.selected.classList.remove('d-none')
-                        this.el.required = false
-
-                        // Create new selected item
-                        let newSelectedItem = this.selectedItem.cloneNode()
-                        newSelectedItem.innerHTML = this.selectedTemplate?.(item) ?? item.id
-                        newSelectedItem.addEventListener('click', _ => {
-                            newSelectedItem.remove()
-                            if (this.selected.children.length === 0) {
-                                this.el.required = true
-                                this.selected.classList.add('d-none')
-                            }
-                        })
-
-                        // Create new hidden input
-                        let multipleInput = this.input.cloneNode()
-                        multipleInput.value = item.id
-
-                        // append selection to selected items container
-                        newSelectedItem.append(multipleInput)
-                        this.selected.append(newSelectedItem)
-
-                        this.el.value = ''
-                        this.searchResults.innerHTML = ''
-
-                    // Else only one selection is allowed
-                    } else {
-                        // set the hidden input value
-                        this.input.value = item.id
-                        this.el.value = this.selectedTemplate?.(item) ?? item.name ?? item.id
-                        this.searchResults.innerHTML = ''
-                        this.searchResults.append(option)
-                    }
-
-                    this.el.dispatchEvent(new Event('keyup'))
-                })
-                this.searchResults.append(option)
-            })
+            data.forEach(item => this.createOptionElement(item))
         })
         .catch(err => {
-            // Log and return error to user
-            this.searchResults.innerHTML = '<span class="text-danger"> there was an error</span>'
+            this.resultsContainer.innerHTML = '<span class="text-danger">there was an error!</span>'
             console.error(err)
         })
     }
