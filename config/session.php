521b1accd7b2b8c78b02687135992462a931b356
Jessseee <jssvssr2000@gmail.com>
2022-04-12 22:37:55 +0200

    Changed default value for session secure cookie config

--- config/session.php
+++ config/session.php
@@ -163,7 +163,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', false),
+    'secure' => env('SESSION_SECURE_COOKIE', null),
 
     /*
     |--------------------------------------------------------------------------
