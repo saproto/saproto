e6e003362f1faf41ed47c4438087bf757a418a40
Jessseee <jssvssr2000@gmail.com>
2022-04-12 23:14:49 +0200

    Revert "Changed default value for session secure cookie config"
    This reverts commit 521b1accd7b2b8c78b02687135992462a931b356.

--- config/session.php
+++ config/session.php
@@ -163,7 +163,7 @@ return [
     |
     */
 
-    'secure' => env('SESSION_SECURE_COOKIE', null),
+    'secure' => env('SESSION_SECURE_COOKIE', false),
 
     /*
     |--------------------------------------------------------------------------
