43fb7ec6c80f214929942d69fc1742dd5596a4e4
Jessseee <jssvssr2000@gmail.com>
2022-04-16 11:00:01 +0200

    Fixed docstring in Factories

--- database/factories/PageFactory.php
+++ database/factories/PageFactory.php
@@ -1,8 +1,9 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factory;
 
-/* @var $factory Closure */
+/* @var $factory Factory */
 $factory->define(
     Proto\Models\Page::class,
     function (Faker $faker) {
