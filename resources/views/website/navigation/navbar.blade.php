98e6907a80777afc2268ff108c0aa1f866259a92
Jessseee <jssvssr2000@gmail.com>
2022-04-21 22:10:59 +0200

    Fixed styling of register button

--- resources/views/website/navigation/navbar.blade.php
+++ resources/views/website/navigation/navbar.blade.php
@@ -335,7 +335,7 @@
                 @else
 
                     <form class="form-inline mt-2 mt-md-0">
-                        <a class="btn btn-outline-light me-5" href="{{ route('login::register') }}">
+                        <a class="btn btn-outline-light me-2" href="{{ route('login::register') }}">
                             <i class="fas fa-user-plus me-2"></i> Register
                         </a>
                         <a class="btn btn-light" href="{{ route('login::show') }}">
