97a9913463a8deec8a95957c88d088a378812013
ysbrandB <ysbrand@burgstede.eu>
2022-10-20 00:28:50 +0200

    added a countdown system and timers

--- app/Http/Controllers/AdventController.php
+++ app/Http/Controllers/AdventController.php
@@ -2,17 +2,21 @@
 
 namespace Proto\Http\Controllers;
 
+use Carbon;
 use Illuminate\Http\Request;
 use Proto\Models\Event;
 
 class AdventController extends Controller
 {
     public function index(){
-        $ids=[[1,2,3,4],[5,6,7,8],[9,10,11,12]];
+        $ids=[[13,725,73,48],[15,61,72,84],[84,34,31,42]];
+        $date=Carbon::create('first day of december')->addHours(12);
+        $date=Carbon::createFromFormat('Y-m-d H:i:s', '2022-10-19 12:05:00');
         $events=[];
         foreach ($ids as $key=>$id){
             $events[$key]=Event::whereIn('id', $id)->get();
         }
-        return view('advent.index',['events'=>$events]);
+        return view('advent.index',['eventsArray'=>$events, 'date'=>$date]);
+
     }
 }
--- resources/views/advent/index.blade.php
+++ resources/views/advent/index.blade.php
@@ -5,20 +5,71 @@
 @endsection
 
 @section('container')
-    <div class="p-5">
+
+    @if($date->timestamp<Carbon::now()->timestamp)
+        <div class="p-5">
         <div class="row justify-content-start">
             <h1>12 'VO</h1>
         </div>
         <div class="row justify-content-between">
-            @foreach($events as $column)
+            @foreach($eventsArray as $colIndex=>$column)
                 <div class="col">
-                    @foreach($column as $event)
+                    @foreach($column as $rowIndex=>$event)
+                        @if($date->addHours($colIndex*4+$rowIndex)->timestamp<Carbon::now()->timestamp)
                         <div>
                             @include('event.display_includes.event_block', ['event'=> $event])
                         </div>
+                        @else
+                            <a class="card mb-3 leftborder leftborder-info text-decoration-none">
+                            <div>
+                                <div unix-time="{{$date->addHours($colIndex*4+$rowIndex)->timestamp}}" class="h1 col text-center countdown">Loading...</div>
+                            </div>
+                            </a>
+                        @endif
+                    {{$colIndex*4+$rowIndex}}
+                        {{$date->addHour($colIndex*4+$rowIndex)->diffForHumans()}}
                     @endforeach
                 </div>
             @endforeach
         </div>
+        </div>
+        @else
+            <div class="container fluid" style="min-height: 100%; min-height: 100vh; display: flex; align-items: center;">
+                <div unix-time="{{$date->timestamp}}" class="h1 col text-center countdown">Loading...</div>
+            </div>
+            @endif
     </div>
 @endsection
+@push('javascript')
+    <script type="text/javascript" nonce="{{ csp_nonce() }}">
+
+        let timers=Array.from(document.getElementsByClassName("countdown"));
+        console.log(timers)
+        timers.forEach((timer)=>{
+        var timing = setInterval( // you're making an interval - a thing, that is updating content after number of miliseconds, that you're writing after comma as second parameter
+        function () {
+        let dt = new Date(timer.getAttribute("unix-time")*1000);
+            let currentDate = new Date().getTime(); //same thing as above
+            let timeLeft = dt - currentDate; //difference between time you set and now in miliseconds
+
+            let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24)); //conversion miliseconds on days
+        if (days < 10) days="0"+days; //if number of days is below 10, programm is writing "0" before 9, that's why you see "09" instead of "9"
+            let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)); //conversion miliseconds on hours
+        if (hours < 10) hours="0"+hours;
+            let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60)); //conversion miliseconds on minutes
+        if (minutes < 10) minutes="0"+minutes;
+            let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);//conversion miliseconds on seconds
+        if (seconds < 10) seconds="0"+seconds;
+
+        timer.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s"; // putting number of days, hours, minutes and seconds in div,
+
+        if (timeLeft <= 0) {
+        clearInterval(timing);
+        window.location.reload();
+        document.getElementById("countdown").innerHTML = "It's over"; //if there's no time left, programm in this 2 lines is clearing interval (nothing is counting now)
+        //and you see "It's over" instead of time left
+        }
+        }, 1000);
+        })
+    </script>
+@endpush
