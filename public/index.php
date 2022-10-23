b0b0ac3ae4026447843bbdcea2898c40d9f5371f
Jessseee <jssvssr2000@gmail.com>
2022-10-23 17:03:05 +0200

    Fixed double declaration of LARAVEL_START

--- public/index.php
+++ public/index.php
@@ -10,8 +10,6 @@
 use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
-
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
