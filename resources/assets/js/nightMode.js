d03617f863577606873ef2269e3e73e528438c01
ysbrandB <ysbrand@burgstede.eu>
2022-11-01 12:17:57 +0100

    remove mobile device check since phones can run the new animations just fine

--- resources/assets/js/nightMode.js
+++ resources/assets/js/nightMode.js
@@ -1,6 +1,4 @@
 window.nightMode = _ => {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) return;
-
     for(let i=0; i<240;i++){
         let star=document.createElement("div")
         star.setAttribute("class", "star");
