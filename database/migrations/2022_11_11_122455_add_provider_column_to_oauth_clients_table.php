ead4bea2efaca20bef4e15aac11012644b59ec1e
ysbrandB <ysbrand@burgstede.eu>
2022-11-11 12:30:41 +0100

    Create 2022_11_11_122455_add_provider_column_to_oauth_clients_table.php
    add migration for the new version of the oAuth library

---
+++ database/migrations/2022_11_11_122455_add_provider_column_to_oauth_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProviderColumnToOauthClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('oauth_clients', 'provider')) {
+            Schema::table('oauth_clients', function (Blueprint $table) {
+                $table->string('provider')->after('secret')->nullable();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('oauth_clients', function (Blueprint $table) {
+            $table->dropColumn('provider');
+        });
+    }
+}
