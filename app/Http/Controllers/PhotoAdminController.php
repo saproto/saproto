8012d6fae656621b241423abfd037ce0dfeb60d9
Jessseee <jssvssr2000@gmail.com>
2022-11-15 20:45:32 +0100

    Fixed references to moved views

--- app/Http/Controllers/PhotoAdminController.php
+++ app/Http/Controllers/PhotoAdminController.php
@@ -105,7 +105,7 @@ class PhotoAdminController extends Controller
             $uploadFile = $request->file('file');
 
             $photo = $this->createPhotoFromUpload($uploadFile, $id);
-            return html_entity_decode(view('website.layouts.macros.selectablephoto', ['photo' => $photo]));
+            return html_entity_decode(view('photos.includes.selectablephoto', ['photo' => $photo]));
             } catch (Exception $e) {
                 return response()->json([
                     'message'=>$e,
