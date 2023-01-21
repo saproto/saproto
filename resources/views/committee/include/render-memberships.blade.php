191f729d4369543d2c9bf1c64a5d85c9eab4ae79
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2023-01-21 11:57:36 +0100

    Update helper notifications (#1842)
    * add a function to end a whole edition of a committee
    
    * Update the helpernotificationcron
    
     To do as much as possible in SQL and add a check to see if a user is still active in the committee
    
    * Apply php-cs-fixer changes
    
    * Removed Dutch comments
    
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>

--- resources/views/committee/include/render-memberships.blade.php
+++ resources/views/committee/include/render-memberships.blade.php
@@ -7,7 +7,11 @@
 
     <div id="committee_collapse_{{ $unique }}" class="collapse {{ $display ? 'show' : null }}"
          data-parent="#committee_collapse">
-
+        @can('board')
+            <div class="card-body">
+               <a href="{{route("committee::membership::endedition", ['edition'=>$edition, 'committee'=>$committee->id])}}" class="float-end"><i class="fas fa-trash me-2 text-danger"></i></a> <span class="float-end">end edition</span>
+            </div>
+        @endcan
         <div class="card-body">
 
             @foreach($memberships as $i => $membership)
