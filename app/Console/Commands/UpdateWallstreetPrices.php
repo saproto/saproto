8e77f5074bde2ccab7b06c58ffb1e34756d86ab9
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2023-04-01 15:18:18 +0200

    Updates to wallstreet marquee screen! (#1911)
    * Start the overhaul of the marquee screen
    
    * restyled the marquee.blade.php to match the new design
    
    * updated margin on title
    
    * make the graph work and update the styling
    
    * Make the percentage based upon the normal price and make the price cap 120% of the normal price
    
    * Apply php-cs-fixer changes
    
    ---------
    
    Co-authored-by: frank <f.bosman-1@student.utwente.nl>
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>

--- app/Console/Commands/UpdateWallstreetPrices.php
+++ app/Console/Commands/UpdateWallstreetPrices.php
@@ -69,10 +69,10 @@ class UpdateWallstreetPrices extends Command
                 $newPriceObject = new WallstreetPrice([
                     'wallstreet_drink_id' => $currentDrink->id,
                     'product_id' => $product->id,
-                    'price' =>$latestPrice->price + $delta >= $product->price ? $product->price : $latestPrice->price + $delta,
+                    'price' =>$latestPrice->price + $delta >= $product->price * 1.2 ? $product->price * 1.2 : $latestPrice->price + $delta,
                 ]);
                 $newPriceObject->save();
-                $this->info($product->id.' has '.$newOrderlines.' new orderlines, increasing price by '.$delta.'to '.$newPriceObject->price);
+                $this->info($product->id.' has '.$newOrderlines.' new orderlines, increasing price by '.$delta.' to '.$newPriceObject->price);
                 continue;
             }
 
