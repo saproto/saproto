d2b73b070a88d4ee39609d1ebf280b6d71aed7f8
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2023-05-15 17:11:08 +0200

    Update the fishcam so it only hits the proxy directly and not the server anymore (#1932)

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -156,25 +156,6 @@ class ApiController extends Controller
         ]);
     }
 
-    /** @return void */
-    public function fishcamStream()
-    {
-        if (env('FISHCAM_URL') == null) {
-            abort(404);
-        }
-
-        header('Content-Transfer-Encoding: binary');
-        header('Content-Type: multipart/x-mixed-replace; boundary=video-boundary--');
-        header('Cache-Control: no-cache');
-        $handle = fopen(env('FISHCAM_URL'), 'r');
-        while ($data = fread($handle, 8192)) {
-            echo $data;
-            ob_flush();
-            flush();
-            set_time_limit(0);
-        }
-    }
-
     /** @return array */
     public function gdprExport()
     {
