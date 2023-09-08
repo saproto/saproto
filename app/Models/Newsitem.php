5eb2a2739a43034aef8f856bc97350e4695a0f98
ysbrandB <ysbrandB@users.noreply.github.com>
2023-09-08 11:13:24 +0000

    PHP Linting (Pint)

--- app/Models/Newsitem.php
+++ app/Models/Newsitem.php
@@ -52,8 +52,8 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
  */
 class Newsitem extends Model
 {
-    use SoftDeletes;
     use HasFactory;
+    use SoftDeletes;
 
     protected $table = 'newsitems';
 
