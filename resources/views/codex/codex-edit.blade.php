d599921a2811a60bb036c3f4ed29e30844ad32e6
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2023-10-05 14:50:03 +0200

    Feature/codex tool (#2001)
    * Start adding the necessary models and migration for the codex tool
    
    * Start implementing the editing of Codices
    
    * Fix saving of codices and add editing of text-types
    
    * add song, text and text-type editing
    
    * Add command to convert wannabe markdown to markdown and try using html2pdf
    
    * Add codex export using html2pdf
    
    * Replace HTML2PDF with FPDF
    
    * Fix text to work with markdown, fix TOC numbers
    
    * Add senate role, update UI, start adding deletion
    
    * Finalize deletion of models and make the delete buttons safer
    
    * Add menu items
    
    * Added text encoding fix to codex markdown converter
    
    * add removal of backslashes
    
    * PHP Linting (Pint)
    
    * Add the FPDF fontpath to the .env examples
    
    * Fixed utf-8 encoding of codex PDFs
    
    * Remove unused codex.blade.php
    
    ---------
    
    Co-authored-by: Jesse Visser <jssvssr2000@gmail.com>
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>
    Co-authored-by: Jesse Visser <j.w.visser-3@student.utwente.nl>

---
+++ resources/views/codex/codex-edit.blade.php
@@ -0,0 +1,38 @@
+@extends('website.layouts.redesign.dashboard')
+
+@section('page-title')
+    Edit codex
+@endsection
+
+@section('container')
+    <form action="{{ isset($codex)&&$codex?route('codex::edit-codex', ['codex'=>$codex]):route("codex::add-codex") }}" method="POST">
+        {{ csrf_field()}}
+        <div class="row gap-3">
+            <div class="col">
+                <div class="row">
+                    @include('codex.includes.codex-details', ['codex'=>$codex])
+                </div>
+                <div class="row">
+                    @include('codex.includes.text_types', ['edit'=>true, 'textTypes' => $textTypes, 'myTextTypes' => $myTextTypes])
+                </div>
+            </div>
+            <div class="col">
+                <div class="row">
+                    <div class="card mb-3 p-3">
+                        <div class="d-inline-flex justify-content-between">
+                            <button type="submit" class="btn btn-success btn-block">
+                                Save codex!
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                <div class="row">
+                        <div class="card-body">
+                            @include('codex.includes.song_list', ['edit'=>true, 'songTypes' => $songTypes, 'mySongs' => $mySongs, 'myShuffles' => $myShuffles])
+                        </div>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
