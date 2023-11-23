e54f84412bb9f83ff485854b1767de27ed59a221
frank <f.bosman-1@student.utwente.nl>
2023-11-23 16:10:51 +0100

    shown active and inactive societies and committees separately

--- resources/views/committee/list.blade.php
+++ resources/views/committee/list.blade.php
@@ -6,13 +6,16 @@
 
 @section('container')
 
+    {{--  Active societies or committees  --}}
     <div class="card mb-3">
-
+        <div class="card-header bg-dark text-white">
+            Active {{$data->first()->is_society ? 'societies' : 'committees'}}
+        </div>
         <div class="card-body">
 
             <div class="row" id="committee__list">
 
-                @foreach($data as $key => $committee)
+                @foreach($data->filter(function($item) {return $item->is_active;}) as $key => $committee)
 
                     <div class="col-lg-2 col-lg-3 col-md-4 col-sm-6">
 
@@ -25,7 +28,30 @@
             </div>
 
         </div>
-
     </div>
+
+    {{--  Inactive societies or committees  --}}
+    @if($data->filter(function($item) {return !$item->is_active;})->count() > 0)
+        <div class="card mb-3">
+            <div class="card-header bg-dark text-white">
+                Inactive {{$data->first()->is_society ? 'societies' : 'committees'}}
+            </div>
+            <div class="card-body">
+                <div class="row" id="committee__list_inactive">
+
+                    @foreach($data->filter(function($item) {return !$item->is_active;}) as $key => $committee)
+
+                        <div class="col-lg-2 col-lg-3 col-md-4 col-sm-6">
+
+                            @include('committee.include.committee_block', ['committee' => $committee, 'photo_pop'=>false])
+
+                        </div>
+
+                    @endforeach
+
+                </div>
+            </div>
+        </div>
+    @endif
 
 @endsection
