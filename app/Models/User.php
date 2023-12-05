ebe7ad70a95e395ba7d33a3560227dca153c09c3
ysbrandB <ysbrandB@users.noreply.github.com>
2023-12-05 20:05:24 +0000

    PHP Linting (Pint)

--- app/Models/User.php
+++ app/Models/User.php
@@ -167,7 +167,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     }
 
     /**
-     * @param string $public_id
+     * @param  string  $public_id
      * @return mixed|User|null
      */
     public static function fromPublicId($public_id)
@@ -184,7 +184,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
      */
     public function isStale()
     {
-        return !(
+        return ! (
             $this->password ||
             $this->edu_username ||
             strtotime($this->created_at) > strtotime('-1 hour') ||
@@ -314,8 +314,8 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     /**
      * Use this method instead of $user->photo->generate to bypass the "no profile" problem.
      *
-     * @param int $w
-     * @param int $h
+     * @param  int  $w
+     * @param  int  $h
      * @return string Path to a resized version of someone's profile picture.
      */
     public function generatePhotoPath($w = 100, $h = 100)
@@ -328,7 +328,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     }
 
     /**
-     * @param string $password
+     * @param  string  $password
      *
      * @throws Exception
      */
@@ -364,10 +364,10 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     public function hasUnpaidOrderlines()
     {
         foreach ($this->orderlines as $orderline) {
-            if (!$orderline->isPayed()) {
+            if (! $orderline->isPayed()) {
                 return true;
             }
-            if ($orderline->orderline && $orderline->withdrawal->id !== 1 && !$orderline->withdrawal->closed) {
+            if ($orderline->orderline && $orderline->withdrawal->id !== 1 && ! $orderline->withdrawal->closed) {
                 return true;
             }
         }
@@ -395,6 +395,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
                 return true;
             }
         }
+
         return false;
     }
 
@@ -409,7 +410,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     }
 
     /**
-     * @param Committee $committee
+     * @param  Committee  $committee
      * @return bool
      */
     public function isInCommittee($committee)
@@ -418,7 +419,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     }
 
     /**
-     * @param string $slug
+     * @param  string  $slug
      * @return bool
      */
     public function isInCommitteeBySlug($slug)
@@ -432,21 +433,21 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     public function isActiveMember()
     {
         return count(
-                CommitteeMembership::withTrashed()
-                    ->where('user_id', $this->id)
-                    ->where('created_at', '<', date('Y-m-d H:i:s'))
-                    ->where(function ($q) {
-                        $q->whereNull('deleted_at')
-                            ->orWhere('deleted_at', '>', date('Y-m-d H:i:s'));
-                    })
-                    ->with('committee')
-                    ->get()
-                    ->where('committee.is_society', false)
-            ) > 0;
+            CommitteeMembership::withTrashed()
+                ->where('user_id', $this->id)
+                ->where('created_at', '<', date('Y-m-d H:i:s'))
+                ->where(function ($q) {
+                    $q->whereNull('deleted_at')
+                        ->orWhere('deleted_at', '>', date('Y-m-d H:i:s'));
+                })
+                ->with('committee')
+                ->get()
+                ->where('committee.is_society', false)
+        ) > 0;
     }
 
     /**
-     * @param int $limit
+     * @param  int  $limit
      * @return Withdrawal[]
      */
     public function withdrawals($limit = 0)
@@ -470,7 +471,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
         if (preg_match("/(?:http|https):\/\/.*/i", $this->website) === 1) {
             return $this->website;
         } else {
-            return 'https://' . $this->website;
+            return 'https://'.$this->website;
         }
     }
 
@@ -577,7 +578,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
         return $this->pref_calendar_alarm;
     }
 
-    /** @param float|null $hours */
+    /** @param  float|null  $hours */
     public function setCalendarAlarm($hours)
     {
         $hours = floatval($hours);
@@ -594,7 +595,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     /** @return void */
     public function toggleCalendarRelevantSetting()
     {
-        $this->pref_calendar_relevant_only = !$this->pref_calendar_relevant_only;
+        $this->pref_calendar_relevant_only = ! $this->pref_calendar_relevant_only;
         $this->save();
     }
 
@@ -607,7 +608,7 @@ class User extends Authenticatable implements AuthenticatableContract, CanResetP
     /** @return bool Whether user has a current membership that is not pending. */
     public function getIsMemberAttribute()
     {
-        return $this->member && !$this->member->is_pending;
+        return $this->member && ! $this->member->is_pending;
     }
 
     /** @return bool */
