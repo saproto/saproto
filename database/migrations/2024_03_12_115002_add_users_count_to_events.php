92121c219a645c5d7cdce8a67761af19bf852be6
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-03-12 23:10:41 +0100

    Also rename spots to participants in the migration

--- database/migrations/2024_03_12_115002_add_users_count_to_events.php
+++ database/migrations/2024_03_12_115002_add_users_count_to_events.php
@@ -5,18 +5,12 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('activities', function (Blueprint $table) {
-            //rename participants to spots
-            $table->renameColumn('participants', 'spots');
-        });
-
         Schema::table('events', function (Blueprint $table) {
             //add a new column that counts the amount of users who signed up called users_count
             $table->unsignedInteger('unique_users_count')->after('publication')->default(0);
@@ -32,11 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('activities', function (Blueprint $table) {
-            //reverse the up
-            $table->renameColumn('spots', 'participants');
-        });
-
         Schema::table('events', function (Blueprint $table) {
             //reverse the up
             $table->dropColumn('unique_users_count');
