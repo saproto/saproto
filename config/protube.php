d43ac9e4d147a948b3da334585c92a56b27c17ed
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-04-08 22:49:21 +0200

    Add the new env variable for the protybe sync that protube expects

--- config/protube.php
+++ config/protube.php
@@ -8,6 +8,6 @@ return [
 
     'server' => env('PROTUBE_SERVER'),
     'secret' => env('PROTUBE_SHARED_SECRET'),
-
+    'laravel_secret' => env('PROTUBE_LARAVEL_SECRET'),
     'remote_url' => env('PROTUBE_REMOTE_URL', 'https://protu.be/remote'),
 ];
