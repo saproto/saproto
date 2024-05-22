9bd13df78ee3dd0715628e909b4e06009a930c11
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2024-05-22 12:26:49 +0200

    Fix the migration for the unique users count (#2155)
    * Fix the migration, somehow the column already exists on the live server so add a check that removes it first when exists and then updates it
    
    * PHP Linting (Pint)
    
    ---------
    
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>

--- database/migrations/2024_03_12_115002_add_users_count_to_events.php
+++ database/migrations/2024_03_12_115002_add_users_count_to_events.php
@@ -12,6 +12,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+
+        if (Schema::hasColumn('events', 'unique_users_count')) {
+            Schema::table('events', function (Blueprint $table) {
+                $table->dropColumn('unique_users_count');
+            });
+        }
+
         Schema::table('events', function (Blueprint $table) {
             //add a new column that counts the amount of users who signed up called users_count for the event_block number
             $table->unsignedInteger('unique_users_count')->after('publication')->default(0);
