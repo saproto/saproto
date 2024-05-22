eccb91abb87957b6b3cd52f8f35f1382ee5c706a
ysbrandB <ysbrandB@users.noreply.github.com>
2024-05-22 10:23:42 +0000

    PHP Linting (Pint)

--- database/migrations/2024_03_12_115002_add_users_count_to_events.php
+++ database/migrations/2024_03_12_115002_add_users_count_to_events.php
@@ -5,13 +5,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        
+
         if (Schema::hasColumn('events', 'unique_users_count')) {
             Schema::table('events', function (Blueprint $table) {
                 $table->dropColumn('unique_users_count');
