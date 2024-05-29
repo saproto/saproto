444be70208de3b3bd8369a53fe277eb4cbcde34a
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-05-29 16:31:17 +0200

    Add caching to pint as well

--- rector.php
+++ rector.php
@@ -9,18 +9,18 @@ use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 
 return RectorConfig::configure()
     ->withCache(
-    // ensure file system caching is used instead of in-memory
+        // ensure file system caching is used instead of in-memory
         cacheDirectory: '/tmp/rector',
 
         // specify a path that works locally as well as on CI job runners
         cacheClass: FileCacheStorage::class
     )
     ->withPaths([
-        __DIR__ . '/app',
-        __DIR__ . '/config',
-        __DIR__ . '/public',
-        __DIR__ . '/resources',
-        __DIR__ . '/routes',
+        __DIR__.'/app',
+        __DIR__.'/config',
+        __DIR__.'/public',
+        __DIR__.'/resources',
+        __DIR__.'/routes',
     ])->withPhpSets(php53: true)
     ->withRules(
         [
