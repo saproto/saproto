ef88e7da2f3a480bc95cbd8184af731fe699e0be
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2024-08-29 19:50:16 +0200

    As discussed in GMM AP 50.15 (Remove the option to become a member through the Discord.) (#2187)
    * As discussed in GMM AP 50.15
    
    This was the only notion of becoming a member in DC I could find, not even something about it in the discord itself
    
    * PHP Linting (Pint)
    
    * Re add the unlocked check
    
    ---------
    
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>

--- app/Http/Controllers/UserDashboardController.php
+++ app/Http/Controllers/UserDashboardController.php
@@ -254,7 +254,7 @@ class UserDashboardController extends Controller
                 'done' => Auth::check() && Auth::user()->is_member,
                 'heading' => 'Become a member!',
                 'icon' => 'fas fa-trophy',
-                'text' => "You're almost a full-fledged Proto member! You'll need to find one of the board-members to finalize your registration. They can usually be found in the Protopolis (Zilverling A230) or on our discord server (invite.gg/proto).",
+                'text' => "You're almost a full-fledged Proto member! You'll need to find one of the board-members to finalize your registration. You can usually find them in the Protopolis (Zilverling A230).",
             ],
             [
                 'url' => route('user::dashboard', ['wizard' => 1]),
