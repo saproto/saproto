76a4c474e560b41bea648daf0e521a32b9f6efe7
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-09-11 16:22:18 +0200

    Add the ICal parser and start looking at the output of time edit

--- app/Http/Controllers/SmartXpScreenController.php
+++ app/Http/Controllers/SmartXpScreenController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Cache;
 use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\View\View;
+use ICal\ICal;
 
 class SmartXpScreenController extends Controller
 {
@@ -29,11 +31,56 @@ class SmartXpScreenController extends Controller
     /** @return array */
     public function timetable()
     {
-        return CalendarController::returnGoogleCalendarEvents(
-            config('proto.google-calendar.timetable-id'),
-            date('c', strtotime('today')),
-            date('c', strtotime('tomorrow'))
-        );
+        $studies = [
+            'Create Year 1' => [
+                'short' => 'CreaTe Y1',
+                'year' => 1,
+                'type' => 'CRE MOD',
+                'ical' => 'https://cloud.timeedit.net/nl_utwente/web/student/ri69dQ34X34ZXtQ5uQ8ZZ074y1Z84YZ6Q929w08QQ62uj6109l9n443j50Z3kj1B21DB136B0m7D9Dt6029QFlAF36Eo944AF6CE26DB.ics',
+            ],
+
+            'Create Year 2' => [
+                'short' => 'CreaTe Y2',
+                'year' => 2,
+                'type' => 'CRE MOD',
+                'ical' => 'https://cloud.timeedit.net/nl_utwente/web/public/ri65Q304518Z5uQ3555X64Q95Z044XZ5n4Q5317Q0Y72y3wXZ86Xn804.ics',
+            ],
+        ];
+
+        foreach ($studies as $study => $value) {
+            $events = Cache::remember("timetable-$study", 3600, function () use ($value) {
+                $ical = new ICal(false, array(
+                    'defaultSpan' => 2,     // Default value
+                    'defaultTimeZone' => 'UTC',
+                    'defaultWeekStart' => 'MO',  // Default value
+                    'disableCharacterReplacement' => false, // Default value
+                    'filterDaysAfter' => null,  // Default value
+                    'filterDaysBefore' => null,  // Default value
+                    'httpUserAgent' => null,  // Default value
+                    'skipRecurrence' => false, // Default value
+                ));
+
+                $ical->initUrl($value['ical']);
+
+                return collect($ical->events());
+            });
+        }
+
+
+        return $events->map(function ($event) {
+            return [
+                'title' => $event->summary,
+                'place' => $event->location,
+                'start' => $event->dtstart_array[2],
+                'end' => $event->dtend_array[2],
+                'type' => null,
+                'year' => null,
+                'study' => null,
+                'studyShort' => null,
+                'over' => strtotime($event->dtend) < time(),
+                'current' => strtotime($event->dtstart) < time() && strtotime($event->dtend) > time(),
+            ];
+        });
     }
 
     /** @return array */
@@ -72,12 +119,12 @@ class SmartXpScreenController extends Controller
             'weekend' => [],
         ];
         $occupied = false;
-        $url = 'https://www.googleapis.com/calendar/v3/calendars/'.config('proto.google-calendar.smartxp-id').'/events?singleEvents=true&orderBy=startTime&key='.config('app-proto.google-key-private').'&timeMin='.urlencode(date('c', strtotime('last monday', strtotime('tomorrow')))).'&timeMax='.urlencode(date('c', strtotime('next monday')));
+        $url = 'https://www.googleapis.com/calendar/v3/calendars/' . config('proto.google-calendar.smartxp-id') . '/events?singleEvents=true&orderBy=startTime&key=' . config('app-proto.google-key-private') . '&timeMin=' . urlencode(date('c', strtotime('last monday', strtotime('tomorrow')))) . '&timeMax=' . urlencode(date('c', strtotime('next monday')));
 
         try {
             $data = json_decode(str_replace('$', '', file_get_contents($url)));
         } catch (Exception $e) {
-            return (object) ['roster' => $roster, 'occupied' => $occupied];
+            return (object)['roster' => $roster, 'occupied' => $occupied];
         }
 
         foreach ($data->items as $entry) {
@@ -90,7 +137,7 @@ class SmartXpScreenController extends Controller
             }
             $name = '';
             foreach ($name_exp as $val) {
-                $name .= $val.' ';
+                $name .= $val . ' ';
             }
             preg_match('/Type: (.*)/', $entry->description, $type);
             $current = strtotime($start_time) < time() && strtotime($end_time) > time();
@@ -98,7 +145,7 @@ class SmartXpScreenController extends Controller
                 $occupied = true;
             }
             $day = strtolower(str_replace(['Saturday', 'Sunday'], ['weekend', 'weekend'], date('l', strtotime($start_time))));
-            $roster[$day][] = (object) [
+            $roster[$day][] = (object)[
                 'title' => $name,
                 'start' => strtotime($start_time),
                 'end' => strtotime($end_time),
@@ -108,13 +155,13 @@ class SmartXpScreenController extends Controller
             ];
         }
 
-        return (object) ['roster' => $roster, 'occupied' => $occupied];
+        return (object)['roster' => $roster, 'occupied' => $occupied];
     }
 
     /** @return View */
     public function canWork()
     {
         return view('smartxp.caniwork', ['timetable' => $this->smartxpTimetable()->roster,
-            'occupied' => $this->smartxpTimetable()->occupied, ]);
+            'occupied' => $this->smartxpTimetable()->occupied,]);
     }
 }
