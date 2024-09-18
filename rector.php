6b935cd7a369f942c6d71e77d82ac6c44160d605
ysbrandB <ysbrandB@users.noreply.github.com>
2024-09-18 16:23:14 +0000

    PHP Linting (Pint)

--- rector.php
+++ rector.php
@@ -18,19 +18,19 @@ use RectorLaravel\Set\LaravelSetList;
 try {
     return RectorConfig::configure()
         ->withCache(
-        // ensure file system caching is used instead of in-memory
+            // ensure file system caching is used instead of in-memory
             cacheDirectory: '.tmp/rector',
 
             // specify a path that works locally as well as on CI job runners
             cacheClass: FileCacheStorage::class
         )
         ->withPaths([
-            __DIR__ . '/app',
-            __DIR__ . '/config',
-            __DIR__ . '/routes',
-            __DIR__ . '/resources/views',
-            __DIR__ . '/tests',
-            __DIR__ . '/database',
+            __DIR__.'/app',
+            __DIR__.'/config',
+            __DIR__.'/routes',
+            __DIR__.'/resources/views',
+            __DIR__.'/tests',
+            __DIR__.'/database',
         ])->withPhpSets(php82: true)
         ->withsets([
             LaravelLevelSetList::UP_TO_LARAVEL_110,
@@ -67,7 +67,7 @@ try {
                 NullToStrictStringFuncCallArgRector::class,
                 AddClosureVoidReturnTypeWhereNoReturnRector::class,
                 RenamePropertyRector::class,
-                __DIR__ . '/app/Libraries',
+                __DIR__.'/app/Libraries',
             ]);
 } catch (InvalidConfigurationException $e) {
 }
