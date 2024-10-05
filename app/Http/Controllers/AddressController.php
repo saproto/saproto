6f8c4147dcd7ec1f27bd7065525b72f2951a2056
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2024-10-06 00:56:58 +0200

    Rename user::adress::show to user::address::create (#2228)
    * Rename user::adress::show to user::address::create
    
    * PHP Automatic Fixing (Pint & Rector)
    
    ---------
    
    Co-authored-by: ysbrandB <ysbrandB@users.noreply.github.com>

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -17,7 +17,7 @@ class AddressController extends Controller
     /**
      * @return View|RedirectResponse
      */
-    public function show(Request $request)
+    public function create(Request $request)
     {
         $user = Auth::user();
 
