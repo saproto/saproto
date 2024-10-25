9c69a8e1e5494ca4e8e074288a6df97d72973546
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-10-26 01:56:14 +0200

    Remove the old .env variables

--- config/ldap.php
+++ config/ldap.php
@@ -3,7 +3,6 @@
 return [
     'proxy' => [
         'utwente' => [
-            'url' => getenv('LDAP_PROXY_URL'),
             'post_url' => getenv('POST_LDAP_PROXY_URL'),
             'key' => getenv('LDAP_PROXY_KEY'),
         ],
