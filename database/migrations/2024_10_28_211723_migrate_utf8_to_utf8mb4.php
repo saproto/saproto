25914758c70b26dc8a3705688efe839c4da37e7b
ysbrandB <ysbrandB@users.noreply.github.com>
2024-10-28 20:49:31 +0000

    PHP Automatic Fixing (Pint & Rector)

--- database/migrations/2024_10_28_211723_migrate_utf8_to_utf8mb4.php
+++ database/migrations/2024_10_28_211723_migrate_utf8_to_utf8mb4.php
@@ -1,11 +1,12 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 //migration taken from https://gist.github.com/hrsa/7a45420329e745315ee02a08ddbf3d41 which itself is forked from https://gist.github.com/NBZ4live/04d5981eaf0244b57d0296b381e04195
 //this migration is used to change the charset of the database from utf8 to utf8mb4 which allows for emojis and other special characters to be stored in the database
-return new class extends Migration {
-
+return new class extends Migration
+{
     public function up(): void
     {
         $this->migrateCharsetTo('utf8mb4', 'utf8mb4_unicode_520_ci');
@@ -36,7 +37,6 @@ return new class extends Migration {
             DB::unprepared("ALTER TABLE {$tableName} CONVERT TO CHARACTER SET {$charset} COLLATE {$collation};");
         }
 
-
         // Get the list of all columns in the active db that have a collation
         $columns = DB::table('information_schema.columns')
             ->where('table_schema', $databaseName)
@@ -52,18 +52,14 @@ return new class extends Migration {
 
             // Skip strange columns with char() type
 
-            if (str_starts_with($columnType, "char(")) {
+            if (str_starts_with($columnType, 'char(')) {
                 continue;
             }
 
             // Check for default value in nullable and not nullable columns
 
             if ($column->IS_NULLABLE == 'NO') {
-                if ($column->COLUMN_DEFAULT !== null) {
-                    $default = "DEFAULT $column->COLUMN_DEFAULT";
-                } else {
-                    $default = 'NOT NULL';
-                }
+                $default = $column->COLUMN_DEFAULT !== null ? "DEFAULT $column->COLUMN_DEFAULT" : 'NOT NULL';
             } elseif ($column->COLUMN_DEFAULT !== null) {
                 $default = "DEFAULT $column->COLUMN_DEFAULT";
             } else {
