807895683f73ecf5537f3cbdcd8869cdd2167f8a
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-10-31 13:29:50 +0100

    Import App in the Molliecontroller

--- app/Http/Controllers/MollieController.php
+++ app/Http/Controllers/MollieController.php
@@ -12,6 +12,7 @@ use Carbon;
 use Exception;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
