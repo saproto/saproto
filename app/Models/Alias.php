2cd78e3da88b03a5ed9d82df4cb024e4ffe93c64
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-11-29 23:09:00 +0100

    Squashed commit of the following:
    commit 60d18b36eee670ce8cb7842b2e983087a931c8ed
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Fri Nov 29 21:16:50 2024 +0100
    
        Fix inactive member alias query (#2295)
    
        * Fix inactive member alias query
    
        * PHP Automatic Fixing (Pint & Rector)
    
        ---------
    
        Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>
    
    commit 4bcc8f70010f96a7bb1a2e1317246ea8dca7c725
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Thu Nov 28 23:37:43 2024 +0100
    
        Fix adding group members (#2292)
    
        * Fix adding group members
    
        * PHP Automatic Fixing (Pint & Rector)
    
        ---------
    
        Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>
    
    commit 99a5824560d057458da9ccc12a66ed051f0e938f
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Thu Nov 28 23:19:19 2024 +0100
    
        Add alias group check before syncing members (#2291)
    
        * Add alias group check before syncing members
    
        * PHP Automatic Fixing (Pint & Rector)
    
        ---------
    
        Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>
    
    commit 153f9fe8fe3373bfecc0d57926d85314ce47c407
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Thu Nov 28 23:05:37 2024 +0100
    
        Fix alias group filtering (#2290)
    
    commit 3a454fcaf3eddb226d8c47b289d1a59a83122679
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Thu Nov 28 22:49:43 2024 +0100
    
        Fix count method call in string (#2289)
    
        * Fix count method call in string
    
        * PHP Automatic Fixing (Pint & Rector)
    
        ---------
    
        Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>
    
    commit 401345b7e440924ba18ed87c1727f88c63473dd4
    Author: Jonathan Matarazzi <git@jonathanm.nl>
    Date:   Thu Nov 28 22:28:48 2024 +0100
    
        Sync aliases as groups (#2287)
    
        * Sync aliases as groups
    
        * PHP Automatic Fixing (Pint & Rector)
    
        * Remove aliases created using old method
    
        * Remove old alias sync code
    
        * PHP Automatic Fixing (Pint & Rector)
    
        * Catch alias group creation errors
    
        * PHP Automatic Fixing (Pint & Rector)
    
        * Add non-members to external aliases sync
    
        * Fix comment and printing error
    
        * PHP Automatic Fixing (Pint & Rector)
    
        * Fix wrong alias property usage
    
        ---------
    
        Co-authored-by: JonaMata <JonaMata@users.noreply.github.com>

--- app/Models/Alias.php
+++ app/Models/Alias.php
@@ -7,6 +7,7 @@ use Eloquent;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Config;
 
 /**
  * Alias Model.
@@ -40,5 +41,10 @@ class Alias extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function getEmailAttribute(): string
+    {
+        return $this->alias.'@'.Config::string('proto.emaildomain');
     }
 }
