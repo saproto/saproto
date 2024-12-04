2c46c78df83ac1d9b91ce579cb71e5012133c1e3
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-12-04 14:52:41 +0100

    Update the emailconfig to Laravel 11 from Laravel 5

--- config/mail.php
+++ config/mail.php
@@ -4,45 +4,92 @@ return [
 
     /*
     |--------------------------------------------------------------------------
-    | Mail Driver
+    | Default Mailer
     |--------------------------------------------------------------------------
     |
-    | Laravel supports both SMTP and PHP's "mail" function as drivers for the
-    | sending of e-mail. You may specify which one you're using throughout
-    | your application here. By default, Laravel is setup for SMTP mail.
-    |
-    | Supported: "smtp", "mail", "sendmail", "mailgun", "mandrill", "ses", "log"
+    | This option controls the default mailer that is used to send all email
+    | messages unless another mailer is explicitly specified when sending
+    | the message. All additional mailers can be configured within the
+    | "mailers" array. Examples of each type of mailer are provided.
     |
     */
 
-    'driver' => env('MAIL_DRIVER', 'log'),
-    'host' => env('SMTP_HOST', 'localhost'),
-    'port' => env('SMTP_PORT', 25),
-    'encryption' => env('SMTP_ENCRYPTION', false),
-    'username' => env('SMTP_USERNAME'),
-    'password' => env('SMTP_PASSWORD'),
+    'default' => env('MAIL_DRIVER', 'log'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Mailer Configurations
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure all of the mailers used by your application plus
+    | their respective settings. Several examples have been configured for
+    | you and you are free to add your own as your application requires.
+    |
+    | Laravel supports a variety of mail "transport" drivers that can be used
+    | when delivering an email. You may specify which one you're using for
+    | your mailers below. You may also add additional mailers if needed.
+    |
+    | Supported: "smtp", "sendmail", "mailgun", "ses", "ses-v2",
+    |            "postmark", "resend", "log", "array",
+    |            "failover", "roundrobin"
+    |
+    */
+
+    'mailers' => [
+
+        'smtp' => [
+            'transport' => 'smtp',
+            'url' => env('MAIL_URL'),
+            'host' => env('SMTP_HOST', '127.0.0.1'),
+            'port' => env('SMTP_PORT', 2525),
+            'encryption' => env('SMTP_ENCRYPTION', false),
+            'username' => env('SMTP_USERNAME'),
+            'password' => env('SMTP_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url(env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+        ],
+
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+
+        'array' => [
+            'transport' => 'array',
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
     | Global "From" Address
     |--------------------------------------------------------------------------
     |
-    | You may wish for all e-mails sent by your application to be sent from
-    | the same address. Here, you may specify a name and address that is
-    | used globally for all e-mails that are sent by your application.
+    | You may wish for all emails sent by your application to be sent from
+    | the same address. Here you may specify a name and address that is
+    | used globally for all emails that are sent by your application.
     |
     */
 
-    'from' => ['address' => 'haveyoutriedturningitoffandonagain@proto.utwente.nl', 'name' => 'S.A. Proto Have You Tried Turning It Off And On Again committee'],
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'haveyoutriedturningitoffandonagain@proto.utwente.nl'),
+        'name' => env('MAIL_FROM_NAME', 'S.A. Proto Have You Tried Turning It Off And On Again committee'),
+    ],
+
 
     /*
-     * In order to provide support for Laravel 5.4's new Markdown mail components,
-     * you should add the following block of configuration to the bottom of your
-     * mail configuration file:
-     */
+    |--------------------------------------------------------------------------
+    | Markdown Mail Settings
+    |--------------------------------------------------------------------------
+    |
+    | If you are using Markdown based email rendering, you may configure your
+    | theme and component paths here, allowing you to customize the design
+    | of the emails. Or, you may simply stick with the Laravel defaults!
+    |
+    */
 
     'markdown' => [
-        'theme' => 'default',
+        'theme' => env('MAIL_MARKDOWN_THEME', 'default'),
+
         'paths' => [
             resource_path('views/vendor/mail'),
         ],
