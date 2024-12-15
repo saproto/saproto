a18c2c92191641433c5b33e6fc2401b3b52d515d
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2024-12-15 15:10:04 +0100

    Fix the local dev setup

--- resources/assets/js/echo.js
+++ resources/assets/js/echo.js
@@ -7,7 +7,7 @@ import Echo from 'laravel-echo';
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
-    wsHost: import.meta.env.VITE_PUSHER_HOST,
+    wsHost: import.meta.env.DEV ? '127.0.0.1' : import.meta.env.VITE_PUSHER_HOST,
     wsPort: import.meta.env.VITE_PUSHER_PORT,
     wssPort: import.meta.env.VITE_PUSHER_PORT,
     forceTLS: false,
