030dc8003bbb47b77d0c5d763ad23ee0e0cf1615
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2025-01-12 17:36:24 +0100

    Fix the Carbon import in the achievementscron

--- app/Console/Commands/AchievementsCron.php
+++ app/Console/Commands/AchievementsCron.php
@@ -13,9 +13,8 @@ use App\Models\OrderLine;
 use App\Models\Product;
 use App\Models\ProductCategory;
 use App\Models\User;
-use Carbon;
-use Carbon\Carbon;
 use Illuminate\Console\Command;
+use Illuminate\Support\Carbon;
 
 class AchievementsCron extends Command
 {
