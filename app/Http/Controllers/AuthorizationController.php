a60910ee2dee678b17b3f7483b68890e381870a5
Burgstede, W.Y. (Ysbrand, Student B-CREA) <w.y.burgstede@student.utwente.nl>
2025-01-13 16:33:06 +0100

    Properly import Role and add types

--- app/Http/Controllers/AuthorizationController.php
+++ app/Http/Controllers/AuthorizationController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Role;
 use App\Models\User;
 use App\Services\ProTubeApiService;
 use Illuminate\Http\RedirectResponse;
@@ -12,11 +11,11 @@ use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 use Illuminate\View\View;
 use Permission;
+use Spatie\Permission\Models\Role;
 
 class AuthorizationController extends Controller
 {
-    /** @return View */
-    public function index()
+    public function index(): view
     {
         $roles = Role::all();
         $permissions = Permission::all();
@@ -24,11 +23,7 @@ class AuthorizationController extends Controller
         return view('authorization.overview', ['roles' => $roles, 'permissions' => $permissions]);
     }
 
-    /**
-     * @param  int  $id
-     * @return RedirectResponse
-     */
-    public function grant(Request $request, $id)
+    public function grant(Request $request, int $id): RedirectResponse
     {
         if ($id == Config::integer('proto.rootrole')) {
             Session::flash('flash_message', 'This role can only be manually added in the database.');
@@ -37,7 +32,7 @@ class AuthorizationController extends Controller
         }
 
         /** @var Role $role */
-        $role = Role::findOrFail($id);
+        $role = Role::query()->findOrFail($id);
         /** @var User $user */
         $user = User::query()->findOrFail($request->user);
 
@@ -57,9 +52,8 @@ class AuthorizationController extends Controller
     /**
      * @param  int  $id  role ID
      * @param  int  $userId  user ID
-     * @return RedirectResponse
      */
-    public function revoke(Request $request, $id, $userId)
+    public function revoke(int $id, int $userId): RedirectResponse
     {
         if ($id == Config::integer('proto.rootrole')) {
             Session::flash('flash_message', 'This role can only be manually removed in the database.');
@@ -68,13 +62,13 @@ class AuthorizationController extends Controller
         }
 
         /** @var Role $role */
-        $role = Role::findOrFail($id);
+        $role = Role::query()->findOrFail($id);
         /** @var User $user */
         $user = User::query()->findOrFail($userId);
         $user->removeRole($role);
 
         // Call Protube webhook to remove this user's admin rights, only remove when role is protube
-        if ($role->name == 'protube') {
+        if ($role->name === 'protube') {
             $updatedAdmin = ProTubeApiService::updateAdmin($user->id, false);
             if (! $updatedAdmin) {
                 Session::flash('flash_message', 'Failed to remove ProTube admin status for '.$user->name.'.');
