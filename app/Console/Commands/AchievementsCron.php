e829f744243c6a9286b39f50d08f568621df0c3b
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-01-14 21:42:01 +0100

    Fix cron query

--- app/Console/Commands/AchievementsCron.php
+++ app/Console/Commands/AchievementsCron.php
@@ -65,7 +65,8 @@ class AchievementsCron extends Command
             ->count();
 
         $youDandy = $this->categoryProducts([9]);
-        $fourOClock = Product::whereIsAlcoholic()->pluck("id")->toArray();
+        $fourOClock = Product::where("is_alcoholic",1)->pluck("id")->toArray();
+        $this->line("Amount of alcoholic: " . count($fourOClock));
 
         $bigKid = $this->categoryProducts([21]);
         $goodHuman = $this->categoryProducts([28]);
