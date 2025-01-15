66df1cef264c6caade8212e5d8969338f458772c
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-01-15 14:51:22 +0100

    Update our node version to 22 (#2323)
    * Update our node version to 22
    
    * Automatic Blade linting (Prettier Blade)

--- resources/views/users/admin/admin_includes/actions.blade.php
+++ resources/views/users/admin/admin_includes/actions.blade.php
@@ -5,24 +5,16 @@
     </div>
 
     <div class="card-body">
-        @includeWhen(
-            ! $user->is_member && ! $user->hasUnpaidOrderlines(),
-            'components.modals.confirm-modal',
-            [
-                'action' => route('user::delete', ['id' => $user->id]),
-                'method' => 'POST',
-                'text' =>
-                    '<button class="btn btn-block btn-danger mb-1"><i class="fas fa-trash"></i> Delete</button>',
-                'title' => 'Confirm Delete',
-                'message' => "Are you sure you want to delete this user's account?
-                                                                                                                                                                                                                                                                                    <div class='form-group mt-2'>
-                                                                                                                                                                                                                                                                                        <label for='confirm-input'>Confirm by typing the users name ($user->name):</label>
-                                                                                                                                                                                                                                                                                        <input type='text' class='form-control' id='confirm-input' name='name'
-                                                                                                                                                                                                                                                                                               value='' placeholder='$user->name' required>
-                                                                                                                                                                                                                                                                                    </div>",
-                'confirm' => 'Delete',
-            ]
-        )
+        @includeWhen
+        ( ! $user->is_member && ! $user->hasUnpaidOrderlines(),
+        'components.modals.confirm-modal', [ 'action' => route('user::delete',
+        ['id' => $user->id]), 'method' => 'POST', 'text' => '
+        <button class="btn btn-block btn-danger mb-1">
+            <i class="fas fa-trash"></i>
+            Delete
+        </button>
+        ', 'title' => 'Confirm Delete', 'message' => "Are you sure you want to
+        delete this user's account? 'confirm' => 'Delete', ] )
 
         <a
             class="btn btn-{{ $user->signed_nda ? 'info' : 'warning' }} btn-block mb-3"
