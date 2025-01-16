97ee2f548d64490148dbe7685969bd5ec60bbd57
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-01-16 15:31:21 +0100

    Remove print statement and use specific whereAlcoholic method

--- app/Console/Commands/AchievementsCron.php
+++ app/Console/Commands/AchievementsCron.php
@@ -65,8 +65,7 @@ class AchievementsCron extends Command
             ->count();
 
         $youDandy = $this->categoryProducts([9]);
-        $fourOClock = Product::where("is_alcoholic",1)->pluck("id")->toArray();
-        $this->line("Amount of alcoholic: " . count($fourOClock));
+        $fourOClock = Product::whereAlcoholic(1)->pluck("id")->toArray();
 
         $bigKid = $this->categoryProducts([21]);
         $goodHuman = $this->categoryProducts([28]);
