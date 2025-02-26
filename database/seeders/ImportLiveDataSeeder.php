b76a49fe1cc11edbfdeacf0633a55513421da084
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-02-26 12:12:50 +0100

    Exclude the image from the seeder

--- database/seeders/ImportLiveDataSeeder.php
+++ database/seeders/ImportLiveDataSeeder.php
@@ -39,7 +39,7 @@ class ImportLiveDataSeeder extends Seeder
             ['name' => 'event_categories'],
             ['name' => 'mailinglists'],
             ['name' => 'menuitems'],
-            ['name' => 'products', 'excluded_columns' => ['image_url']],
+            ['name' => 'products', 'excluded_columns' => ['image_url', 'image']],
             ['name' => 'products_categories'],
             ['name' => 'product_categories'],
             ['name' => 'tickets'],
