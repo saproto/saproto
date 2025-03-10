dfa46aafaa41ec75baa193d70c26476a96fc487e
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-03-10 16:29:53 +0000

    PHP Automatic Fixing (Pint & Rector)

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -1,12 +1,11 @@
 <?php
 
 namespace App\Models;
-
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 
 /**
