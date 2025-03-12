84d1c1d23e0d6036da028fccb29bb38705c63afb
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-03-12 09:35:57 +0000

    PHP Automatic Fixing (Pint & Rector)

--- tests/Feature/EventVisibilityTest.php
+++ tests/Feature/EventVisibilityTest.php
@@ -5,37 +5,37 @@ use App\Models\Member;
 use App\Models\User;
 
 dataset('users', [
-    'user'=> [fn() => User::factory()->create(), false],
-    'member'=>[fn() => User::factory()->has(Member::factory())->create(), false],
-    'board'=>[fn() => User::factory()->has(Member::factory())->create()->givePermissionTo('board'), true],
+    'user' => [fn () => User::factory()->create(), false],
+    'member' => [fn () => User::factory()->has(Member::factory())->create(), false],
+    'board' => [fn () => User::factory()->has(Member::factory())->create()->givePermissionTo('board'), true],
 ]);
 
 dataset('private events', [
-    'secret'=> [fn()=>Event::factory()->create([
+    'secret' => [fn () => Event::factory()->create([
         'secret' => true,
-        'start'=>Carbon::now()->addDay()->timestamp,
-        'end'=>Carbon::now()->addDays(2)->timestamp,
-    ]), "This event is not shown on the site"],
-    'unpublished'=> [fn()=>Event::factory()->create([
+        'start' => Carbon::now()->addDay()->timestamp,
+        'end' => Carbon::now()->addDays(2)->timestamp,
+    ]), 'This event is not shown on the site'],
+    'unpublished' => [fn () => Event::factory()->create([
         'secret' => false,
-        'start'=>Carbon::now()->addDay()->timestamp,
-        'end'=>Carbon::now()->addDays(2)->timestamp,
+        'start' => Carbon::now()->addDay()->timestamp,
+        'end' => Carbon::now()->addDays(2)->timestamp,
         'publication' => Carbon::now()->addHours(2)->timestamp,
-    ]),"This event is scheduled"]
+    ]), 'This event is scheduled'],
 ]);
 
 dataset('public events', [
-    'normal'=> [fn()=>Event::factory()->create([
+    'normal' => [fn () => Event::factory()->create([
         'secret' => false,
-        'start'=>Carbon::now()->addDay()->timestamp,
-        'end'=>Carbon::now()->addDays(2)->timestamp,
+        'start' => Carbon::now()->addDay()->timestamp,
+        'end' => Carbon::now()->addDays(2)->timestamp,
     ])],
-    'published'=> [fn()=>Event::factory()->create([
+    'published' => [fn () => Event::factory()->create([
         'secret' => false,
-        'start'=>Carbon::now()->addDay()->timestamp,
-        'end'=>Carbon::now()->addDays(2)->timestamp,
+        'start' => Carbon::now()->addDay()->timestamp,
+        'end' => Carbon::now()->addDays(2)->timestamp,
         'publication' => Carbon::now()->subHours(2)->timestamp,
-    ])]
+    ])],
 ]);
 
 it('lets everyone see non secret events', function (User $user, bool $unimportant, Event $event) {
@@ -45,8 +45,8 @@ it('lets everyone see non secret events', function (User $user, bool $unimportan
 
     $response->assertSee($event->title);
     $response->assertSee($event->location);
-    $response->assertDontSee("This event is not shown on the site");
-    $response->assertDontSee("This event is scheduled");
+    $response->assertDontSee('This event is not shown on the site');
+    $response->assertDontSee('This event is scheduled');
 
     $response = $this->actingAs($user)
         ->get(route('event::index'));
@@ -69,9 +69,9 @@ it('lets everyone see the right events', function (User $user, bool $canSeeEvent
         ->get(route('event::index'));
     $response->assertStatus(200);
 
-    if($canSeeEventInOverview){
+    if ($canSeeEventInOverview) {
         $response->assertSee($event->title);
-    }else{
+    } else {
         $response->assertDontSee($event->title);
     }
 })->with('users', 'private events');
