c3bb255e151f143d0b0516f1304c90e2057f387a
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-05-28 12:33:32 +0200

    Seperate the admin and normal layouts

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
 import Menubar from '@/components/Menubar.vue'
 import { router } from '@inertiajs/vue3'
-import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
-import AppSidebar from '@/components/AppSidebar.vue'
 router.on('invalid', (event) => {
     event.preventDefault()
     const response = event.detail.response
@@ -13,12 +11,8 @@ router.on('invalid', (event) => {
 <template>
     <div class="min-h-screen w-full">
         <Menubar />
-        <SidebarProvider>
-            <AppSidebar />
             <main>
-                <SidebarTrigger />
                 <slot />
             </main>
-        </SidebarProvider>
     </div>
 </template>
