9d6969952c995f59b4f12c9b4d0898b2f3524893
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-06-02 15:58:51 +0200

    Add a card and start adding blueeee

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
 import { Button } from '@/components/ui/button'
 import AppLayout from '@/layouts/AppLayout.vue'
-
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
 function handleClick() {
     console.log('Button clicked!')
 }
@@ -15,6 +22,19 @@ const page = usePage()
 </script>
 
 <template>
+
     {{ page.props }}
     <Button @click="handleClick">Click me</Button>
+    <Card>
+        <CardHeader>
+            <CardTitle>Card Title</CardTitle>
+            <CardDescription>Card Description</CardDescription>
+        </CardHeader>
+        <CardContent>
+            Card Content
+        </CardContent>
+        <CardFooter>
+            Card Footer
+        </CardFooter>
+    </Card>
 </template>
