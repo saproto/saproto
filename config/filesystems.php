6b20915d3cc70af77e146326f76ff3cadcb93594
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-06-21 14:51:02 +0200

    Let our file system reflect the default of laravel and add the public drive in the storage folder (#2416)

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,12 +29,35 @@ return [
     */
 
     'disks' => [
-
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app'),
+            'root' => storage_path('app/private'),
+            'serve' => true,
+            'throw' => false,
+            'report' => false,
         ],
 
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+            'throw' => false,
+            'report' => false,
+        ],
     ],
+    /*
+     |--------------------------------------------------------------------------
+     | Symbolic Links
+     |--------------------------------------------------------------------------
+     |
+     | Here you may configure the symbolic links that will be created when the
+     | `storage:link` Artisan command is executed. The array keys should be
+     | the locations of the links and the values should be their targets.
+     |
+     */
 
+    'links' => [
+        public_path('storage') => storage_path('app/public'),
+    ],
 ];
