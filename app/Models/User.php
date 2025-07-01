76082f454b65885e5aa3c03e0e3eafae2fae7679
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-07-01 15:38:33 +0000

    PHP Automatic Fixing (Pint & Rector)

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,7 +17,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
