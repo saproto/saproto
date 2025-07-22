be7059d02cda56a17e28c42aa3420fcee8e036e3
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-07-22 19:05:06 +0200

    Set the name of the file to photo_{$id}

--- app/Http/Controllers/PhotoAdminController.php
+++ app/Http/Controllers/PhotoAdminController.php
@@ -110,7 +110,9 @@ class PhotoAdminController extends Controller
 
         $disk = $album->private ? 'local' : 'public';
         try {
-            $photo->addMediaFromRequest('file')->toMediaCollection(diskName: $disk);
+            $photo->addMediaFromRequest('file')
+                ->usingFileName('photo_'.$photo->id)
+                ->toMediaCollection(diskName: $disk);
 
             return html_entity_decode(view('photos.includes.selectablephoto', ['photo' => $photo]));
         } catch (FileDoesNotExist|FileIsTooBig $e) {
