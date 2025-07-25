ece7aaaa4388712019ff0d7a78a0b30a689690e7
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-07-25 09:15:52 +0000

    PHP Automatic Fixing (Pint & Rector)

--- database/factories/PhotoFactory.php
+++ database/factories/PhotoFactory.php
@@ -4,7 +4,6 @@ namespace Database\Factories;
 
 use App\Models\Photo;
 use App\Models\PhotoAlbum;
-use App\Models\StorageEntry;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Carbon;
 use Override;
