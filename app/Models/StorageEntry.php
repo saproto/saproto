5ea912a7c3f88eb2deaa236843372f8eac5d1ed8
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-07-25 11:05:12 +0200

    remove the photo file_id from the storage entry

--- app/Models/StorageEntry.php
+++ app/Models/StorageEntry.php
@@ -70,7 +70,6 @@ class StorageEntry extends Model
             Newsitem::query()->where('featured_image_id', $this->id)->count() == 0 &&
             SoundboardSound::query()->where('file_id', $this->id)->count() == 0 &&
             HeaderImage::query()->where('image_id', $this->id)->count() == 0 &&
-            Photo::query()->withoutGlobalScopes()->where('file_id', $this->id)->count() == 0 &&
             Member::query()->where('omnomcom_sound_id', $this->id)->count() == 0 &&
             WallstreetEvent::query()->where('image_id', $this->id)->count() == 0 &&
             Sticker::query()->where('file_id', $this->id)->count() == 0;
