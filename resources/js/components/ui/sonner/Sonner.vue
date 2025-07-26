2bc7381fc923044e1a5943f1fe1a1fea6bdfc9fe
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-07-26 18:26:32 +0200

    Update Sonner.vue

--- resources/js/components/ui/sonner/Sonner.vue
+++ resources/js/components/ui/sonner/Sonner.vue
@@ -5,14 +5,13 @@ const props = defineProps<ToasterProps>()
 </script>
 
 <template>
-  <Sonner
-    class="toaster group"
-    v-bind="props"
-    :style="{
-      '--normal-bg': 'var(--popover)',
-      '--normal-text': 'var(--popover-foreground)',
-      '--normal-border': 'var(--border)',
-
-    }"
-  />
+    <Sonner
+        class="toaster group"
+        v-bind="props"
+        :style="{
+            '--normal-bg': 'var(--popover)',
+            '--normal-text': 'var(--popover-foreground)',
+            '--normal-border': 'var(--border)',
+        }"
+    />
 </template>
