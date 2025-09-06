2dd220ec5457f90123e77888cc2f2ca2d5f668fd
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-09-06 18:38:35 +0200

    Cleanup soft deleted pages and their files (#2462)

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -9,7 +9,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 /**
@@ -53,8 +52,6 @@ class Page extends Model
 {
     /** @use HasFactory<PageFactory>*/
     use HasFactory;
-
-    use SoftDeletes;
 
     protected $table = 'pages';
 
