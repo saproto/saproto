00658576224973987ac79a1d33982d9d6cf9c4a4
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-09-10 00:56:23 +0200

    Remove the code for the pages storage entries

--- app/Models/StorageEntry.php
+++ app/Models/StorageEntry.php
@@ -58,7 +58,6 @@ class StorageEntry extends Model
     {
         return
             Page::query()->where('featured_image_id', $this->id)->count() == 0 &&
-            DB::table('pages_files')->where('file_id', $this->id)->count() == 0 &&
             Member::withTrashed()->where('membership_form_id', $this->id)->count() == 0 &&
             DB::table('emails_files')->where('file_id', $this->id)->count() == 0 &&
             Newsitem::query()->where('featured_image_id', $this->id)->count() == 0 &&
