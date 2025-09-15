4df29f6b7de3be87b8998491e88927d8116da3d2
Pringles <80323633+PringlePot@users.noreply.github.com>
2025-09-15 21:40:25 +0200

    Oops, add missing file

---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+    <footer class="mt-5 bg-green-500 px-5 py-10 border-t-5 border-green-600 rounded-lg">
+        <img
+            height="130px"
+            width="234.7px"
+            src='/images/logo/inverse.png'
+            alt="inverse proto logo"
+        />
+        <p class="text-center font-bold text-white mt-5 text-sm">
+            &copy; 2025 SaProto. All rights reserved.
+        </p>
+    </footer>
+</template>
