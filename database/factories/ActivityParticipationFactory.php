e9ea54ca1a25d83f679f2ef4683ceb5a83bb3118
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-10-26 20:28:58 +0100

    Prevent seeder crashes when event has bad dates.
    This commit makes it so when the signup start is after the start of the
    activity, the seeder sends a warning and sets the signup date to the
    start date of the activity.
    
    This behaviour is not fully correct and the activity should be corrected
    on the live site, thats why it sends a warning.

--- database/factories/ActivityParticipationFactory.php
+++ database/factories/ActivityParticipationFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use App\Models\Activity;
 use App\Models\ActivityParticipation;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Date;
 use Override;
@@ -24,7 +25,7 @@ class ActivityParticipationFactory extends Factory
         return [
             'created_at' => fn ($attributes): string => self::createAt($attributes),
             'deleted_at' => fn ($attributes): ?string => fake()->boolean(30) ? self::deletedAt($attributes) : null,
-            'backup' => fn ($attributes): bool => self::Backup($attributes),
+            'backup' => fn ($attributes): bool => self::backup($attributes),
         ];
     }
 
@@ -38,8 +39,13 @@ class ActivityParticipationFactory extends Factory
         $start = Date::parse($activity->registration_start);
         $end = Date::parse($activity->event->start);
 
-        $date = fake()->dateTimeBetween($start, $end);
-
+        try {
+            $date = fake()->dateTimeBetween($start, $end);
+        } catch(Exception $e ) {
+            echo "\nWarning, error occured: ". $e->getMessage(). "\n";
+            echo "When creating participation for activity: '" . $activity->event->title . "' (" . $activity->event->id . ")\n" ;
+            $date = $end;
+        }
         return $date->format('Y-m-d H:i:s');
     }
 
