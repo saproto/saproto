e12416059001a63fdf49f804714ebc111af0745e
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-11-03 17:01:17 +0100

    Add DTO's to the product and orderline for omnomcom wrapped

---
+++ app/Data/OrderlineData.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Data;
+
+use Spatie\LaravelData\Data;
+
+/** @typescript */
+class OrderlineData extends Data
+{
+    public function __construct(
+        public int $id,
+        public int $product_id,
+        public int $units,
+        public int $total_price,
+        public string $created_at,
+        public ProductData $product,
+    ) {}
+}
