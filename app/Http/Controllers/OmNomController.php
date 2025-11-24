29b35e7ee60ef5bef04eb666f30e3cdef95f4828
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-11-24 16:47:25 +0000

    PHP Automatic Fixing (Pint & Rector)

--- app/Http/Controllers/OmNomController.php
+++ app/Http/Controllers/OmNomController.php
@@ -13,26 +13,16 @@ use App\Models\User;
 use App\Services\ProTubeApiService;
 use Exception;
 use Illuminate\Contracts\Database\Query\Builder;
-use Illuminate\Contracts\Pagination\CursorPaginator;
-use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Pagination\AbstractCursorPaginator;
-use Illuminate\Pagination\AbstractPaginator;
-use Illuminate\Support\Collection;
-use Illuminate\Support\Enumerable;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\LazyCollection;
 use Illuminate\View\View;
-use Spatie\LaravelData\CursorPaginatedDataCollection;
-use Spatie\LaravelData\DataCollection;
-use Spatie\LaravelData\PaginatedDataCollection;
 use stdClass;
 
 class OmNomController extends Controller
