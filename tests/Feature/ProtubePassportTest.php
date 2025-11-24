b44a69e53fbe981d1da7224af7de8331661948b7
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2025-11-24 17:16:39 +0000

    PHP Automatic Fixing (Pint & Rector)

--- tests/Feature/ProtubePassportTest.php
+++ tests/Feature/ProtubePassportTest.php
@@ -5,18 +5,18 @@ use App\Models\User;
 use Laravel\Passport\Passport;
 
 test('lists-user', function () {
-$user = User::factory()->create();
-Passport::actingAs($user, ['*']);
+    $user = User::factory()->create();
+    Passport::actingAs($user, ['*']);
 
-$response = $this->getJson(route('api::protube::'));
+    $response = $this->getJson(route('api::protube::'));
 
-$response->assertOk()
-    ->assertJson([
-        'authenticated' => true,
-        'name' => $user->calling_name,
-        'admin' => false,
-        'id' => $user->id,
-    ])
+    $response->assertOk()
+        ->assertJson([
+            'authenticated' => true,
+            'name' => $user->calling_name,
+            'admin' => false,
+            'id' => $user->id,
+        ])
         ->assertJsonStructure([
             'authenticated',
             'name',
