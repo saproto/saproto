d7fb14cbcde35dfcacdf71b867d95b869c6780a0
Jakub Stachurski <j.stachurski@student.utwente.nl>
2025-12-08 22:13:13 +0100

    Create the participant management modal

--- resources/views/event/display_includes/render_participant_list.blade.php
+++ resources/views/event/display_includes/render_participant_list.blade.php
@@ -22,11 +22,99 @@
         </a>
         @if (Auth::user()->can('board') && $event && ! $event->activity->closed)
             <a
-                href="{{ route('event::deleteparticipation', ['participation' => $pid]) }}"
-                class="btn btn-outline-warning"
-            >
-                <i class="fas fa-times" aria-hidden="true"></i>
+                class="btn btn-outline-warning participation-admin-modal-button"
+                title="Manage participation"
+                href="#"
+                data-bs-toggle="modal"
+                data-bs-target="#participation-admin-modal"
+                data-participation="{{ $pid }}"
+                data-user-name="{{ $u->name }}"
+                data-user="{{ $u->id }}"
+                >
+                <i class="fas fa-pencil" aria-hidden="true"></i>
             </a>
         @endif
     </div>
 @endforeach
+
+
+@once
+    @push('modals')
+        <div class="modal" id="participation-admin-modal" tabindex="-1" role="dialog">
+            <div class="modal-dialog model-sm" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Manage Participation for ...</h5>
+                        <button
+                                type="button"
+                                class="btn-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                            ></button>
+
+                    </div>
+                    <div class="modal-body">
+                        <div class="d-flex flex-column">
+                            <label class="mb-1"for="pam-unparticipate"><b>Remove this user from the participant list</b></label>
+                            <a
+                                href="#"                             class="btn btn-outline-warning"
+                                id="pam-unparticipate"
+                                <i class="fas fa-times" aria-hidden="true"></i> Remove
+                            </a>
+                        </div>
+                        <div class="d-flex flex-column">
+                            <form method="POST" action="{{route('event:swap_participation')}}">
+                                {{ csrf_field() }}
+                                <input id="pam-swap-from" name="from" class="d-none" type="number", value="0"/>
+                                <input id="pam-swap-pid" name="pid" class="d-none" type="number", value="0"/>
+                                <label class="mb-1"for="pam-unparticipate"><b>Swap this user with another user</b></label>
+                                <div class="row mb-3">
+                                    <div class="col-9">
+                                    <div class="form-group autocomplete ">
+                                        <input class="form-control user-search" name="to" required />
+                                    </div>
+                                    </div >
+                                    <div class="col-3">
+                                        <button
+                                        class="btn btn-outline-primary btn-block"
+                                        type="submit"
+                                        >
+                                            <i class="fas fa-arrow-right-arrow-left"></i>
+                                        </button>
+
+                                    </div >
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endpush
+@endonce
+
+@once
+    @push('javascript')
+        <script nonce="{{ csp_nonce() }}">
+           document.querySelectorAll('.participation-admin-modal-button').forEach((el) => el.addEventListener('click',(e) => {
+                const modal = document.querySelector(
+                    el.getAttribute('data-bs-target')
+                );
+                const pid = el.getAttribute('data-participation');
+                const uid = el.getAttribute('data-user');
+                const username = el.getAttribute('data-user-name');
+
+                modal.querySelector('.modal-title').innerHTML = "Manage participation for " +  username + ".";
+
+                modal.querySelector('#pam-unparticipate').href = "{{ route('event::deleteparticipation', ['participation' => '_pid_']) }}".replace('_pid_', pid)
+
+                modal.querySelector('#pam-swap-from').value = uid;
+                modal.querySelector('#pam-swap-pid').value = pid;
+
+            } ));
+        </script>
+    @endpush
+@endonce
