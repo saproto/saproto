8c5063114d8185f3dba04f383cfd65897ad38937
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-01-25 17:54:11 +0100

    Add the Sentry\Laravel\Integration import

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,32 +1,36 @@
+@php
+    use Sentry\Laravel\Integration;
+@endphp
+
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        {!! Integration::sentryMeta() !!}
+    {!! Integration::sentryMeta()!!}
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <link
-            rel="shortcut icon"
-            href="{{ asset('images/favicons/favicon' . mt_rand(1, 4) . '.png') }}"
-        />
-        <link
-            rel="search"
-            type="application/opensearchdescription+xml"
-            title="S.A. Proto"
-            href="{{ route('search::opensearch') }}"
-        />
+    <link
+        rel="shortcut icon"
+        href="{{ asset('images/favicons/favicon' . mt_rand(1, 4) . '.png') }}"
+    />
+    <link
+        rel="search"
+        type="application/opensearchdescription+xml"
+        title="S.A. Proto"
+        href="{{ route('search::opensearch') }}"
+    />
 
-        <!-- Scripts -->
-        @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
+    <!-- Scripts -->
+    @vite(['resources/js/app.ts', "resources/js/pages/{$page['component']}.vue"])
 
-        @inertiaHead
+    @inertiaHead
 
-        @section('opengraph')
+    @section('opengraph')
         <meta property="og:url" content="{{ Request::url() }}" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="@yield('page-title', 'Website')" />
@@ -38,12 +42,12 @@
             property="og:image"
             content="@yield('og-image', asset('images/logo/og-image.png'))"
         />
-        @show
-    </head>
-    <body
-        class="bg-background text-foreground min-h-screen font-sans antialiased"
-        data-theme="{{ config('proto.themes')[Auth::user()?->theme ?? 0] }}"
-    >
-        @inertia
-    </body>
+    @show
+</head>
+<body
+    class="bg-background text-foreground min-h-screen font-sans antialiased"
+    data-theme="{{ config('proto.themes')[Auth::user()?->theme ?? 0] }}"
+>
+@inertia
+</body>
 </html>
