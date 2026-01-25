fe0ddd689d84c44b359a39588d8a631ddefc545b
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-01-25 21:19:09 +0100

    Make it so locally we can use in memory sqlite for testing, but on CI mariaDB

--- database/migrations/2025_08_09_181322_remove_use_dark_theme_from_users.php
+++ database/migrations/2025_08_09_181322_remove_use_dark_theme_from_users.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,9 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('use_dark_theme');
-        });
+        if (!DB::connection() instanceof SQLiteConnection) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('use_dark_theme');
+            });
+        }
     }
 
     /**
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 use Override;
 use Spatie\Permission\PermissionRegistrar;
 
@@ -14,6 +16,20 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
         $this->withoutVite();
         $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        if (DB::connection() instanceof SQLiteConnection) {
+            $db = DB::connection()->getPdo();
+
+            // 1. Fix SqLite not having the FROM_UNIXTIME function
+            $db->sqliteCreateFunction('FROM_UNIXTIME', function ($value) {
+                return date('Y-m-d H:i:s', $value);
+            });
+
+            // 2. Fix SqLite not having the YEAR function
+            $db->sqliteCreateFunction('YEAR', function ($value) {
+                return date('Y', strtotime($value));
+            });
+        }
 
     }
 }
