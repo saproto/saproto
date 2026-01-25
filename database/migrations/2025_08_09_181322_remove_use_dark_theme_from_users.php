dbe658a7e58f2ae25b20c8ab725335764378b2e0
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-01-25 22:07:17 +0000

    PHP Automatic Fixing (Pint & Rector)

--- database/migrations/2025_08_09_181322_remove_use_dark_theme_from_users.php
+++ database/migrations/2025_08_09_181322_remove_use_dark_theme_from_users.php
@@ -3,8 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\SQLiteConnection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
-use \Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
