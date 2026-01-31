b3e71b849a247918c808ea535e7ab8e0fefc8bd1
Menkveld <menke.veerman@gmail.com>
2026-02-01 00:05:40 +0100

    Add db copy

---
+++ staging/copydb.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+set -euo pipefail
+
+#BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+BRANCH="test"
+
+if [[ $BRANCH == "staging" ]]; then
+    echo "Not cloning branch on staging"
+    exit 0
+fi
+
+SRC_DB="staging"
+DST_DB="app_dev"
+MYSQL_ROOT_USER="${DB_ROOT_USER:-root}"
+
+if [ -z "$MYSQL_ROOT_PWD" ]; then
+  echo "MYSQL_ROOT_PWD is not set."
+  exit 1
+fi
+
+echo "Cloning $SRC_DB -> $DST_DB (host: $DB_HOST)"
+
+# recreate destination db
+mariadb --skip-ssl -h "$DB_HOST" -P "$DB_PORT" -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PWD" \
+  -e "DROP DATABASE IF EXISTS \`$DST_DB\`; CREATE DATABASE \`$DST_DB\`;"
+
+# dump + restore
+mariadb-dump --skip-ssl -h "$DB_HOST" -P "$DB_PORT" -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PWD" "$SRC_DB" \
+  | mariadb --skip-ssl -h "$DB_HOST" -P "$DB_PORT" -u"$MYSQL_ROOT_USER" -p"$MYSQL_ROOT_PWD" "$DST_DB"
+
+mariadb --skip-ssl -h "$DB_HOST" -P "$DB_PORT" -u"$MYSQL_ROOT_USER" \
+   -p"$MYSQL_ROOT_PWD" -e \
+   "GRANT ALL PRIVILEGES ON $DST_DB.* TO '$DB_USERNAME'@'%';"
+
+echo "Done."
