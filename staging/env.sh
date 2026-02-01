c8a21ca7fd3e752f1c3bb3cc84f7470d84b888f9
Menkveld <menke.veerman@gmail.com>
2026-02-01 15:12:06 +0100

    update envsh

--- staging/env.sh
+++ staging/env.sh
@@ -3,13 +3,17 @@
 
 set -eu
 
+echo "Sourcing staging/env.sh"
+
 # Only compute BRANCH if COOLIFY_BRANCH exists and BRANCH isn't already set
 if [ -n "${COOLIFY_BRANCH:-}" ] && [ -z "${BRANCH:-}" ]; then
   BRANCH="$(printf '%s' "$COOLIFY_BRANCH" | tr -d '"' | tr '/' '_')"
+  echo "Setting BRANCH to '$BRANCH'"
   export BRANCH
 fi
 
 # Only set DB_DATABASE if we have a branch and it's not already set
 if [ -n "${BRANCH:-}" ] && [ -z "${DB_DATABASE:-}" ]; then
+  echo "Setting DB_DATABASE to 'db_${BRANCH}'"
   export DB_DATABASE="db_${BRANCH}"
 fi
