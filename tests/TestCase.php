b895e75eaac7d6280a4f39dde70a2d89cd3e0730
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-02-04 12:30:30 +0100

    Make local testing way faster by using in memory sqlite (#2549)
    * Make it so locally we can use in memory sqlite for testing, but on CI mariaDB
    
    * PHP Automatic Fixing (Pint & Rector)
    
    * Import the DB facade
    
    * PHP Automatic Fixing (Pint & Rector)
    
    * Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,7 +2,9 @@
 
 namespace Tests;
 
+use Illuminate\Database\SQLiteConnection;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 use Override;
 use Spatie\Permission\PermissionRegistrar;
 
@@ -14,6 +16,16 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
         $this->withoutVite();
         $this->app->make(PermissionRegistrar::class)->forgetCachedPermissions();
+
+        if (DB::connection() instanceof SQLiteConnection) {
+            $db = DB::connection()->getPdo();
+
+            // Fix SqLite not having the FROM_UNIXTIME function
+            $db->sqliteCreateFunction('FROM_UNIXTIME', fn ($value) => date('Y-m-d H:i:s', $value));
+
+            // Fix SqLite not having the YEAR function
+            $db->sqliteCreateFunction('YEAR', fn ($value) => date('Y', strtotime((string) $value)));
+        }
 
     }
 }
