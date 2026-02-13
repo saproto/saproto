9e539fd77a634af65a1aeeed794070befadfd05c
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-02-13 11:34:02 +0100

    Push the Flash and Error modals to the modal stack

--- resources/views/components/modals/errormessages.blade.php
+++ resources/views/components/modals/errormessages.blade.php
@@ -1,22 +1,25 @@
 @if (isset($errors) && count($errors->all()) > 0)
-    <div class="modal fade" id="flash-modal" tabindex="-1" role="dialog">
-        <div
-            class="modal-dialog modal-dialog-centered modal-lg"
-            role="document"
-        >
-            <div class="modal-content bg-danger text-white">
-                <div class="modal-body text-center">
-                    <h5 class="text-center">Whoops</h5>
-                    @foreach ($errors->all() as $e)
-                        <div>
-                            <i class="fas fa-exclamation-triangle me-2"></i>
-                            {{ $e }}
-                        </div>
-                    @endforeach
+    @push('modals')
+        <div class="modal fade" id="flash-modal" tabindex="-1" role="dialog">
+            <div
+                class="modal-dialog modal-dialog-centered modal-lg"
+                role="document"
+            >
+                <div class="modal-content bg-danger text-white">
+                    <div class="modal-body text-center">
+                        <h5 class="text-center">Whoops</h5>
+                        @foreach ($errors->all() as $e)
+                            <div>
+                                <i class="fas fa-exclamation-triangle me-2"></i>
+                                {{ $e }}
+                            </div>
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endpush
+
     @push('javascript')
         <script type="text/javascript" @cspNonce>
             window.addEventListener('load', () => {
--- resources/views/components/modals/flashmessages.blade.php
+++ resources/views/components/modals/flashmessages.blade.php
@@ -1,16 +1,18 @@
 @if (Session::has('flash_message'))
-    <div class="modal fade" id="flash-modal" tabindex="-1" role="dialog">
-        <div
-            class="modal-dialog modal-dialog-centered modal-lg"
-            role="document"
-        >
-            <div class="modal-content bg-dark text-white">
-                <div class="modal-body text-center">
-                    {!! Session::get('flash_message') !!}
+    @push('modals')
+        <div class="modal fade" id="flash-modal" tabindex="-1" role="dialog">
+            <div
+                class="modal-dialog modal-dialog-centered modal-lg"
+                role="document"
+            >
+                <div class="modal-content bg-dark text-white">
+                    <div class="modal-body text-center">
+                        {!! Session::get('flash_message') !!}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endpush
 
     @push('javascript')
         <script type="text/javascript" @cspNonce>
