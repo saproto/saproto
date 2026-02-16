843c860cdbc9a65210d1a4506406f361ad584b56
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-02-16 14:25:21 +0100

    Fix the buy_limit being null when no buy limit is set

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -60,7 +60,7 @@ class TicketController extends Controller
         $ticket->product_id = Product::query()->findOrFail($request->input('product'))->id;
         $ticket->members_only = $request->has('is_members_only');
         $ticket->has_buy_limit = $request->has('has_buy_limit');
-        $ticket->buy_limit = $request->input('buy_limit', $ticket->buy_limit);
+        $ticket->buy_limit = $request->input('buy_limit') ?? 0;
         $ticket->is_prepaid = $request->has('is_prepaid');
         $ticket->available_from = $request->date('available_from')->timestamp;
         $ticket->available_to = $request->date('available_to')->timestamp;
