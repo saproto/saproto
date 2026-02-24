19f3d7362e7784161a7c7cfa6de1a05a68ba7d9c
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-02-24 10:14:35 +0100

    Remove @eslint/eslintrc and update @sentry/vite-plugin

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,68 +1,60 @@
-const {
-    defineConfig,
-    globalIgnores,
-} = require("eslint/config");
+const { defineConfig, globalIgnores } = require("eslint/config");
 
 const globals = require("globals");
 const parser = require("vue-eslint-parser");
 const vue = require("eslint-plugin-vue");
-const typescriptEslint = require("@typescript-eslint/eslint-plugin");
+const tsPlugin = require("@typescript-eslint/eslint-plugin");
+const tsParser = require("@typescript-eslint/parser");
 const js = require("@eslint/js");
+const prettier = require("eslint-config-prettier");
 
-const {
-    FlatCompat,
-} = require("@eslint/eslintrc");
-
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-module.exports = defineConfig([{
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+module.exports = defineConfig([
+    js.configs.recommended,
+    {
+        plugins: {
+            "@typescript-eslint": tsPlugin,
+        },
+        rules: tsPlugin.configs.recommended.rules,
+    },
+    prettier,
+    ...vue.configs["flat/recommended"],
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
+            parser,
+            ecmaVersion: "latest",
+            parserOptions: {
+                parser: tsParser,
+            },
         },
 
-        parser: parser,
-        ecmaVersion: "latest",
+        plugins: {
+            vue,
+            "@typescript-eslint": tsPlugin,
+        },
 
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
+        rules: {
+            "no-undef": "off",
+            "vue/multi-word-component-names": "off",
+            "@typescript-eslint/no-explicit-any": "off",
         },
     },
 
-    extends: [...compat.extends(
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ),
-        vue.configs["flat/recommended"],
-
-    ],
-
-    plugins: {
-        vue,
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    rules: {
-        "no-undef": "off",
-        "vue/multi-word-component-names": "off",
-        "@typescript-eslint/no-explicit-any": "off"
-    },
-},
     {
         files: ["resources/js/components/ui/**/*.vue"],
         rules: {
             "vue/require-default-prop": "off",
-            "@typescript-eslint/no-unused-vars": "off"
+            "@typescript-eslint/no-unused-vars": "off",
         },
-    },globalIgnores([
+    },
+
+    globalIgnores([
         "**/node_modules/",
         "**/dist/",
         "**/js/actions/",
         "**/js/routes/",
-        "**/js/wayfinder/"])
+        "**/js/wayfinder/",
+    ]),
 ]);
