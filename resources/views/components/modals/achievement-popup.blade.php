8637946c239da1ddce7d82d44a18276634e7666f
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-03-07 16:02:45 +0100

    Push the newachievement modal to the modal stack

--- resources/views/components/modals/achievement-popup.blade.php
+++ resources/views/components/modals/achievement-popup.blade.php
@@ -18,36 +18,39 @@
 @endphp
 
 @if (isset($newAchievements) && count($newAchievements) > 0)
-    <div
-        class="modal fade"
-        id="new-achievement-modal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="newAchievementModalLabel"
-    >
+    @push('modals')
         <div
-            class="modal-dialog modal-dialog-centered modal-lg"
-            role="document"
+            class="modal fade"
+            id="new-achievement-modal"
+            tabindex="-1"
+            role="dialog"
+            aria-labelledby="newAchievementModalLabel"
         >
-            <div class="modal-content bg-dark text-white">
-                <div class="modal-body text-center">
-                    You just got
-                    {{ count($newAchievements) > 1 ? 'new achievements' : 'a new achievement' }},
-                    check it out:
-                    @foreach ($newAchievements as $newAchievement)
-                        @include('achievement.includes.achievement_include', ['achievement' => $newAchievement, 'obtained' => $newAchievement->pivot])
-                    @endforeach
+            <div
+                class="modal-dialog modal-dialog-centered modal-lg"
+                role="document"
+            >
+                <div class="modal-content bg-dark text-white">
+                    <div class="modal-body text-center">
+                        You just got
+                        {{ count($newAchievements) > 1 ? 'new achievements' : 'a new achievement' }},
+                        check it out:
+                        @foreach ($newAchievements as $newAchievement)
+                            @include('achievement.includes.achievement_include', ['achievement' => $newAchievement, 'obtained' => $newAchievement->pivot])
+                        @endforeach
 
-                    <a
-                        class="btn btn-success"
-                        href="{{ route('user::profile') }}"
-                    >
-                        View all my achievements
-                    </a>
+                        <a
+                            class="btn btn-success"
+                            href="{{ route('user::profile') }}"
+                        >
+                            View all my achievements
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endpush
+
     @push('javascript')
         <script type="text/javascript" @cspNonce>
             window.addEventListener('load', () => {
