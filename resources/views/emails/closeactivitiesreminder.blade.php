a2759e6ac2218d7d51bac2c2e557ab2f837d03f6
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-05-18 21:26:00 +0000

    Automatic linting (Prettier and eslint)

--- resources/views/emails/closeactivitiesreminder.blade.php
+++ resources/views/emails/closeactivitiesreminder.blade.php
@@ -4,7 +4,8 @@
     <p>Hey!</p>
 
     <p>
-        There are {{ $unclosed->count() }} activities that do not have been closed, even though they ended more than a month ago!
+        There are {{ $unclosed->count() }} activities that do not have been
+        closed, even though they ended more than a month ago!
         <br />
         Make sure to close them
         <a href="{{ route('event::financial::list') }}">here.</a>
@@ -13,24 +14,26 @@
     <p>
         It is about the following activities:
 
-    @foreach ($unclosed as $activity)
-        <table
-            style="
+        @foreach ($unclosed as $activity)
+            <table
+                style="
                     margin: 0;
                     padding: 0;
                     border: none;
                     background-color: #f0f0f0;
                     width: 100%;
                 "
-        >
-            <tr style="margin: 0; padding: 0; border: none">
+            >
+                <tr style="margin: 0; padding: 0; border: none">
                     <p>
-                        {{ $activity->event->title }} (€{{ $activity->price }})
+                        {{ $activity->event->title }}
+                        (€{{ $activity->price }})
                     </p>
-                <hr />
-            </tr>
-        </table>
+                    <hr />
+                </tr>
+            </table>
         @endforeach
+    </p>
 
-        <p>Kind regards, The Activity Close Clerk</p>
-        @endsection
+    <p>Kind regards, The Activity Close Clerk</p>
+@endsection
