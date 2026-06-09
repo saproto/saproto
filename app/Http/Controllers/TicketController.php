c0f83a435352c7eb4667fc4724ce93e719527b36
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-06-09 14:23:17 +0200

    Also remove the ticket warning for board when updating tickets for externals

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -81,12 +81,6 @@ class TicketController extends Controller
      */
     public function update(Request $request, int $id)
     {
-        if (! $request->has('is_members_only') && ! $request->has('is_prepaid') && ! Auth::user()->can('sysadmin')) {
-            Session::flash('flash_message', 'Making tickets for external people payable via withdrawal is risky and usually not necessary. If you REALLY want this, please contact the Have You Tried Turninig It Off And On Again committee.');
-
-            return back();
-        }
-
         /** @var Ticket $ticket */
         $ticket = Ticket::query()->findOrFail($id);
 
