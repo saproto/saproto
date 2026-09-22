03e48a1547801ee5fcfe81aeebde7d8e23d78b66
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-09-22 15:30:33 +0000

    PHP Automatic Fixing (Pint & Rector)

--- app/Models/EmailListSubscription.php
+++ app/Models/EmailListSubscription.php
@@ -7,7 +7,6 @@ use Illuminate\Database\Eloquent\Attributes\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Support\Carbon;
 
 /**
