a3a15ec1611a4a96cb18ed37d37e85601236fd8e
ysbrandB <72889753+ysbrandB@users.noreply.github.com>
2026-09-22 16:08:54 +0200

    Add frame-src self to the basic csp rules (#2752)

--- app/Csp/Presets/Basic.php
+++ app/Csp/Presets/Basic.php
@@ -16,6 +16,7 @@ class Basic implements Preset
             ->add(Directive::CONNECT, Keyword::SELF)
             ->add(Directive::DEFAULT, Keyword::SELF)
             ->add(Directive::FRAME_ANCESTORS, Keyword::SELF)
+            ->add(Directive::FRAME, Keyword::SELF)
             ->add(Directive::FORM_ACTION, Keyword::SELF)
             ->add(Directive::IMG, Keyword::SELF)
             ->add(Directive::MEDIA, Keyword::SELF)
